49768147b28ff7ee3e1497bbe33f5231860be464
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2019-11-18 13:36:26 +0100

    Fixed port

--- cloudera/tests/conftest.py
+++ cloudera/tests/conftest.py
@@ -13,7 +13,7 @@ def sts_environment(instance_dev):
 def instance_dev():
     return {
         'url': 'http://ec2-34-244-15-117.eu-west-1.compute.amazonaws.com',
-        'port': '7180',
+        'port': 7180,
         'username': 'cloudera',
         'password': 'v4APBoEqW4',
         'api_version': 'v18'
