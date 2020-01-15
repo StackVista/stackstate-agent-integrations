f4604681aea24a5dbd0eeab2acaf2ebc65fe111f
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2020-01-15 16:04:11 +0100

    AwsClient wrapper around boto3 calls

--- aws/stackstate_checks/aws/aws.py
+++ aws/stackstate_checks/aws/aws.py
@@ -25,10 +25,6 @@ DEFAULT_BOTO3_CONFIG = Config(
 TRACES_API_ENDPOINT = 'http://localhost:8126/v0.3/traces'
 
 
-def get_current_region():
-    return boto3.session.Session().region_name
-
-
 class AwsCheck(AgentCheck):
     INSTANCE_TYPE = 'aws'
 
@@ -36,56 +32,20 @@ class AwsCheck(AgentCheck):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
         self.log.setLevel(logging.INFO)
         self.trace_ids = {}
-        self.account_id = None
-        self.aws_access_key_id = None
-        self.aws_secret_access_key = None
         self.region = None
+        self.account_id = None
 
     def get_instance_key(self, instance):
         return TopologyInstance(self.INSTANCE_TYPE, self.account_id)
 
     def check(self, instance):
-        self.aws_access_key_id = instance.get('aws_access_key_id')
-        self.aws_secret_access_key = instance.get('aws_secret_access_key')
-        role_arn = instance.get('role_arn')
-        self.region = instance.get('region')
-        if self.aws_secret_access_key and self.aws_access_key_id and role_arn and self.region:
-            sts_client = boto3.client('sts', config=DEFAULT_BOTO3_CONFIG, aws_access_key_id=self.aws_access_key_id,
-                                      aws_secret_access_key=self.aws_secret_access_key)
-            role = sts_client.assume_role(RoleArn=role_arn, RoleSessionName='xray-client')
-            self.aws_access_key_id = role['Credentials']['AccessKeyId']
-            self.aws_secret_access_key = role['Credentials']['SecretAccessKey']
-            session_token = role['Credentials']['SessionToken']
+        aws_client = AwsClient(instance)
+        self.region = aws_client.region
+        self.account_id = aws_client.get_account_id()
 
-            self.account_id = self._get_account_id(session_token)
-
-            traces = self._get_xray_traces(session_token)
-
-            headers = {'Content-Type': 'application/json'}
-            requests.put(TRACES_API_ENDPOINT, data=json.dumps(traces), headers=headers)
-        else:
-            # TODO: agent service check
-            pass
-
-    def _get_xray_traces(self, session_token):
-        xray_client = boto3.client('xray', region_name=self.region, config=DEFAULT_BOTO3_CONFIG,
-                                   aws_access_key_id=self.aws_access_key_id,
-                                   aws_secret_access_key=self.aws_secret_access_key,
-                                   aws_session_token=session_token)
-
-        # TODO: for development timedelta is 1 hour, it should be 1 minute
-        start_time = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
-        end_time = datetime.datetime.utcnow()
-        operation_params = {'StartTime': start_time, 'EndTime': end_time}
-        trace_summaries = []
         traces = []
-
-        for page in xray_client.get_paginator('get_trace_summaries').paginate(**operation_params):
-            for trace_summary in page['TraceSummaries']:
-                trace_summaries.append(trace_summary)
-
-        for trace_summary in trace_summaries:
-            xray_traces = xray_client.batch_get_traces(TraceIds=[trace_summary['Id']])
+        xray_traces_batch = aws_client.get_xray_traces()
+        for xray_traces in xray_traces_batch:
             for xray_trace in xray_traces['Traces']:
                 trace = []
                 for segment in xray_trace['Segments']:
@@ -93,13 +53,17 @@ class AwsCheck(AgentCheck):
                     trace.extend(self._generate_spans(segment_documents))
                 traces.append(trace)
 
-        return traces
+        headers = {'Content-Type': 'application/json'}
+        requests.put(TRACES_API_ENDPOINT, data=json.dumps(traces), headers=headers)
 
     def _generate_spans(self, segments, trace_id=None, parent_id=None):
         """Translates X-Ray trace to StackState trace."""
         spans = []
+        pp = PrettyPrinter(indent=2)
 
         for segment in segments:
+            pp.pprint(segment)
+
             span_id = int(segment['id'], 16)
             start = datetime.datetime.utcfromtimestamp(segment['start_time'])
             try:
@@ -112,6 +76,12 @@ class AwsCheck(AgentCheck):
             if not trace_id:
                 trace_id = self._convert_trace_id(segment['trace_id'])
 
+            # find resource type
+            try:
+                resource_type = segment['origin']
+            except KeyError:
+                resource_type = segment['name']
+
             # find service name
             try:
                 service_name = segment['resource_arn']
@@ -119,36 +89,17 @@ class AwsCheck(AgentCheck):
                 try:
                     service_name = segment['aws']['function_arn']
                 except KeyError:
-                    try:
-                        service_name = segment['aws']['operation']
-                    except KeyError:
-                        service_name = segment['name']
+                    service_name = segment['name']
 
-            # find resource type
-            try:
-                resource_type = segment['origin']
-            except KeyError:
-                resource_type = segment['name']
+            if 'arn:' not in service_name:
+                arn = self._generate_arn(resource_type, segment)
+                if arn:
+                    service_name = arn
 
             try:
                 span_parent_id = int(segment['parent_id'], 16)
             except KeyError:
                 span_parent_id = parent_id
-
-            # fix service name
-            if 'arn:' not in service_name:
-                # arn:partition:service:region:account-id:resource-id
-                # arn:partition:service:region:account-id:resource-type/resource-id
-                # arn:partition:service:region:account-id:resource-type:resource-id
-                service, resource_format, aws_key = get_service(resource_type)
-                resource = None
-                if service:
-                    try:
-                        resource = resource_format.format(segment['aws'][aws_key])
-                    except KeyError:
-                        pass
-                    if resource:
-                        service_name = 'arn:aws:{0}:{1}:{2}:{3}'.format(service, self.region, self.account_id, resource)
 
             # times format is nanoseconds from the unix epoch
             span = {
@@ -162,10 +113,10 @@ class AwsCheck(AgentCheck):
                 'parent_id': span_parent_id
             }
 
-            pp = PrettyPrinter(indent=2)
-            pp.pprint(flatten(segment, reducer=dot_reducer))
+            # flatten_all([segment])
 
             self.log.debug(span)
+            pp.pprint(span)
             # TODO: filter traces based on resource type
             spans.append(span)
 
@@ -183,34 +134,99 @@ class AwsCheck(AgentCheck):
             self.trace_ids[aws_trace_id] = trace_id
         return trace_id
 
-    def _get_account_id(self, aws_session_token):
+    def _generate_arn(self, resource_type, segment):
+        """Generates ARN on one of the following patterns:
+        arn:partition:service:region:account-id:resource-id
+        arn:partition:service:region:account-id:resource-type/resource-id
+        arn:partition:service:region:account-id:resource-type:resource-id
+        """
+        arn = None
+
+        service = None
+        resource_format = None
+        aws_key = None
+        resource = None
+
+        if resource_type in ['AWS::Lambda::Function', 'AWS::Lambda', 'Lambda']:
+            service = 'lambda'
+            resource_format = 'function:{}'
+            aws_key = 'function_name'
+        elif resource_type == 'AWS::Kinesis::Stream':
+            service = 'kinesis_stream'
+        elif resource_type == 'AWS::S3::Bucket':
+            service = 's3'
+        elif resource_type == 'AWS::RDS::DBInstance':
+            service = 'rds'
+        elif resource_type == 'AWS::SNS::Topic':
+            service = 'sns'
+        elif resource_type == 'AWS::SQS::Queue':
+            service = 'sqs'
+        elif resource_type in ['AWS::DynamoDB::Table', 'AWS::DynamoDB', 'DynamoDB']:
+            service = 'dynamodb'
+            resource_format = 'table/{}'
+            aws_key = 'table_name'
+        elif resource_type == 'AWS::EC2::Instance':
+            service = 'ec2'
+        elif resource_type == 'remote':
+            service = 'remote'
+
+        if service:
+            try:
+                resource = resource_format.format(segment['aws'][aws_key])
+            except KeyError:
+                pass
+            if resource:
+                arn = 'arn:aws:{0}:{1}:{2}:{3}'.format(service, self.region, self.account_id, resource)
+            else:
+                arn = segment['aws']['operation']
+
+        return arn
+
+
+class AwsClient:
+    def __init__(self, instance):
+        self.aws_session_token = None
+        aws_access_key_id = instance.get('aws_access_key_id')
+        aws_secret_access_key = instance.get('aws_secret_access_key')
+        role_arn = instance.get('role_arn')
+        self.region = instance.get('region')
+
+        if aws_secret_access_key and aws_access_key_id and role_arn and self.region:
+            sts_client = boto3.client('sts', config=DEFAULT_BOTO3_CONFIG, aws_access_key_id=aws_access_key_id,
+                                      aws_secret_access_key=aws_secret_access_key)
+            role = sts_client.assume_role(RoleArn=role_arn, RoleSessionName='sts-agent-check')
+            self.aws_access_key_id = role['Credentials']['AccessKeyId']
+            self.aws_secret_access_key = role['Credentials']['SecretAccessKey']
+            self.aws_session_token = role['Credentials']['SessionToken']
+
+    def get_account_id(self):
         return boto3.client('sts', config=DEFAULT_BOTO3_CONFIG,
                             aws_access_key_id=self.aws_access_key_id,
                             aws_secret_access_key=self.aws_secret_access_key,
-                            aws_session_token=aws_session_token).get_caller_identity().get('Account')
+                            aws_session_token=self.aws_session_token).get_caller_identity().get('Account')
 
+    def get_xray_traces(self):
+        xray_client = boto3.client('xray', region_name=self.region, config=DEFAULT_BOTO3_CONFIG,
+                                   aws_access_key_id=self.aws_access_key_id,
+                                   aws_secret_access_key=self.aws_secret_access_key,
+                                   aws_session_token=self.aws_session_token)
 
-def get_service(resource_type):
-    if resource_type in ['AWS::Lambda::Function', 'AWS::Lambda', 'Lambda']:
-        return 'lambda', 'function:{}', 'function_name'
-    elif resource_type == 'AWS::Kinesis::Stream':
-        return 'kinesis_stream', None, None
-    elif resource_type == 'AWS::S3::Bucket':
-        return 's3', None, None
-    elif resource_type == 'AWS::RDS::DBInstance':
-        return 'rds', None, None
-    elif resource_type == 'AWS::SNS::Topic':
-        return 'sns', None, None
-    elif resource_type == 'AWS::SQS::Queue':
-        return 'sqs', None, None
-    elif resource_type in ['AWS::DynamoDB::Table', 'AWS::DynamoDB', 'DynamoDB']:
-        return 'dynamodb', 'table/{}', 'table_name'
-    elif resource_type == 'AWS::EC2::Instance':
-        return 'ec2', None, None
-    elif resource_type == 'remote':
-        return 'remote', None, None
-    else:
-        return None, None, None
+        # TODO: for development timedelta is 1 hour, it should be 1 minute
+        start_time = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+        end_time = datetime.datetime.utcnow()
+        operation_params = {'StartTime': start_time, 'EndTime': end_time}
+
+        trace_summaries = []
+        traces = []
+
+        for page in xray_client.get_paginator('get_trace_summaries').paginate(**operation_params):
+            for trace_summary in page['TraceSummaries']:
+                trace_summaries.append(trace_summary)
+
+        for trace_summary in trace_summaries:
+            traces.append(xray_client.batch_get_traces(TraceIds=[trace_summary['Id']]))
+
+        return traces
 
 
 def dot_reducer(key1, key2):
@@ -218,3 +234,11 @@ def dot_reducer(key1, key2):
         return key2
     else:
         return '{}.{}'.format(key1, key2)
+
+
+def flatten_all(segments):
+    for segment in segments:
+        if 'subsegments' in segment.keys():
+            segment['subsegments'] = flatten_all(segment['subsegments'])
+        segment = flatten(segment, dot_reducer)
+    return segments
