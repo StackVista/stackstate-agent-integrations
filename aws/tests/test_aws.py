4c544664b2e0b37e369694d01f80b9815c1d0f26
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2020-01-27 15:43:09 +0100

    Committing other unit tests

--- aws/tests/test_aws.py
+++ aws/tests/test_aws.py
@@ -18,10 +18,8 @@ class MockAwsClient():
 
     @staticmethod
     def get_xray_traces():
-        file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data', 'get_xray_traces.json')
-        with open(file_name) as file:
-            trace = file.read()
-            return jsonpickle.decode(trace)
+        traces = get_file('get_xray_traces.json')
+        return traces
 
     @staticmethod
     def get_account_id():
@@ -29,19 +27,43 @@ class MockAwsClient():
 
 
 def test_traces(aggregator, instance):
-    check = AwsCheck('aws', {}, {})
+    check = AwsCheck('test', {}, {})
     aws_client = MockAwsClient({})
     check.region = aws_client.region
     check.account_id = aws_client.get_account_id()
     traces = check._process_xray_traces(aws_client)
 
-    assert len(traces) == 2
+    assert len(traces) == 3
     assert len(traces[0]) == 25
     assert len(traces[1]) == 25
+    assert len(traces[2]) == 5
 
 
 @patch('stackstate_checks.aws.aws.AwsClient', MockAwsClient)
 def test_service_check(aggregator, instance):
-    aws_check = AwsCheck('aws', {}, {})
+    aws_check = AwsCheck('test', {}, {})
     aws_check.check(instance)
     aggregator.assert_service_check('aws.can_connect', aws_check.CRITICAL)
+
+
+def test_span_generation():
+    check = AwsCheck('test', {}, {})
+    segment = get_file('segment.json')
+    spans = check._generate_spans([segment])
+    assert len(spans) == 9
+
+
+def test_error_trace():
+    check = AwsCheck('test', {}, {})
+    segments = get_file('segments_error.json')
+    spans = check._generate_spans(segments)
+    assert len(spans) == 5
+    assert spans[0]['error'] == 1
+    assert spans[1]['error'] == 1
+
+
+def get_file(file_name):
+    file_with_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data', file_name)
+    with open(file_with_path, 'r') as file:
+        object_from_json_file = jsonpickle.decode(file.read())
+    return object_from_json_file
