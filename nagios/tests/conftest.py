1862cede40711df71b5ea9012629cfd90b956b64
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2020-05-25 18:19:42 +0200

    fixed e2e topology gathering

--- nagios/tests/conftest.py
+++ nagios/tests/conftest.py
@@ -13,10 +13,14 @@ from .common import INSTANCE_INTEGRATION, HERE
 @pytest.fixture(scope='session')
 def sts_environment():
     nagios_conf = os.path.join(HERE, 'compose', 'nagios4', 'nagios.cfg')
+    localhost_conf = os.path.join(HERE, 'compose', 'nagios4', 'localhost.cfg')
+    mysql_conf = os.path.join(HERE, 'compose', 'nagios4', 'mysql.cfg')
     with TempDir("nagios_var_log") as nagios_var_log:
         e2e_metadata = {
             'docker_volumes': [
                 '{}:/etc/nagios/nagios.cfg'.format(nagios_conf),
+                '{}:/opt/nagios/etc/objects/localhost.cfg'.format(localhost_conf),
+                '{}:/opt/nagios/etc/conf.d/mysql.cfg'.format(mysql_conf),
                 '{}:/opt/nagios/var/log/'.format(nagios_var_log),
             ]
         }
