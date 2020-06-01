a9989c3abacc9e9666c7e8c4f85434847ccf31d1
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2020-06-01 16:37:48 +0200

    deleting e2e test module, we wont do it DataDog way

--- nagios/tests/test_e2e.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.mark.e2e
-def test_e2e(sts_agent_check, instance):
-    # This integration is based on tailing logs but because there is an agent running
-    # in the agent image we cannot guarantee that we will pick specific metrics on a test.
-    # The test will fail if the integration cannot read nagios config or related files
-    sts_agent_check(instance, rate=True)
