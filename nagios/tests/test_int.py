772fd58f76ca4ab884fde259c62c7076e272e14b
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2020-06-05 09:04:12 +0200

    removed test_int module

--- nagios/tests/test_int.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from .common import CHECK_NAME
-from stackstate_checks.nagios import NagiosCheck
-
-
-@pytest.mark.usefixtures('sts_environment')
-def test_real_topology(sts_agent_check, integration_instance):
-    sts_agent_check(integration_instance, rate=True)
