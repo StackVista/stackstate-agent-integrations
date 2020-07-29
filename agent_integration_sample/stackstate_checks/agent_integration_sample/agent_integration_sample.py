027f12a3b5d8e3095b27b715a2888a51055bd512
Zandre Witte <zandre.witte@gmail.com>
2020-07-29 21:29:34 +0200

    extends integration sample, added tests for integration sample, fix telemetry stream_id api

--- agent_integration_sample/stackstate_checks/agent_integration_sample/agent_integration_sample.py
+++ agent_integration_sample/stackstate_checks/agent_integration_sample/agent_integration_sample.py
@@ -12,7 +12,6 @@ seed(1)
 
 
 class AgentIntegrationSampleCheck(AgentCheck):
-
     def get_instance_key(self, instance):
         return AgentIntegrationInstance("agent-integration", "sample")
 
@@ -24,23 +23,65 @@ class AgentIntegrationSampleCheck(AgentCheck):
         # gets the value of the `timeout` property or defaults `default_timeout` and casts it to a float data type
         timeout = float(instance.get('timeout', default_timeout))
 
-        self.component("urn:example/host:this_host", "Host", {
-            "name": "this-host",
-            "domain": "Webshop",
-            "layer": "Hosts",
-            "identifiers": ["another_identifier_for_this_host"],
-            "labels": ["host:this_host", "region:eu-west-1"],
-            "environment": "Production"
-        })
-
         some_application_cpu_usage = MetricStream("Host CPU Usage", "host.cpu.usage",
                                                   conditions={"hostname": "this-host"},
                                                   unit_of_measure="Percentage",
                                                   aggregation="MEAN",
                                                   priority="HIGH")
-        some_application_cpu_usage.identifier()
-        cpu_max_average_check = MetricHealthChecks.maximum_average(some_application_cpu_usage.identifier(),
-                                                                   "Max CPU Usage", 75, 90)
+        cpu_max_average_check = MetricHealthChecks.maximum_average(some_application_cpu_usage.identifier,
+                                                                   "Max CPU Usage (Average)", 75, 90)
+        cpu_max_last_check = MetricHealthChecks.maximum_last(some_application_cpu_usage.identifier,
+                                                             "Max CPU Usage (Last)", 75, 90)
+        cpu_min_average_check = MetricHealthChecks.minimum_average(some_application_cpu_usage.identifier,
+                                                                   "Min CPU Usage (Average)", 10, 5)
+        cpu_min_last_check = MetricHealthChecks.minimum_last(some_application_cpu_usage.identifier,
+                                                             "Min CPU Usage (Last)", 10, 5)
+        self.component("urn:example/host:this_host", "Host",
+                       data={
+                            "name": "this-host",
+                            "domain": "Webshop",
+                            "layer": "Hosts",
+                            "identifiers": ["another_identifier_for_this_host"],
+                            "labels": ["host:this_host", "region:eu-west-1"],
+                            "environment": "Production"
+                       },
+                       streams=[some_application_cpu_usage],
+                       checks=[cpu_max_average_check, cpu_max_last_check, cpu_min_average_check, cpu_min_last_check])
+
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+
+        some_application_2xx_responses = MetricStream("2xx Responses", "2xx.responses",
+                                                      conditions={"application": "some_application",
+                                                                  "region": "eu-west-1"},
+                                                      unit_of_measure="Count",
+                                                      aggregation="MEAN",
+                                                      priority="HIGH")
+        some_application_5xx_responses = MetricStream("5xx Responses", "5xx.responses",
+                                                      conditions={"application": "some_application",
+                                                                  "region": "eu-west-1"},
+                                                      unit_of_measure="Count",
+                                                      aggregation="MEAN",
+                                                      priority="HIGH")
+        max_response_ratio_check = MetricHealthChecks.maximum_ratio(some_application_2xx_responses.identifier,
+                                                                    some_application_5xx_responses.identifier,
+                                                                    "OK vs Error Responses",
+                                                                    50, 75)
+        max_percentile_response_check = MetricHealthChecks.maximum_percentile(some_application_5xx_responses.identifier,
+                                                                              "Error Response 99th Percentile",
+                                                                              50, 70, 99)
+        failed_response_ratio_check = MetricHealthChecks.failed_ratio(some_application_2xx_responses.identifier,
+                                                                      some_application_5xx_responses.identifier,
+                                                                      "OK vs Error Responses",
+                                                                      50, 75)
+        min_percentile_response_check = MetricHealthChecks.minimum_percentile(some_application_2xx_responses.identifier,
+                                                                              "Success Response 99th Percentile",
+                                                                              10, 5, 99)
         self.component("urn:example/application:some_application", "Application",
                        data={
                             "name": "some-application",
@@ -51,12 +92,20 @@ class AgentIntegrationSampleCheck(AgentCheck):
                             "environment": "Production",
                             "version": "0.2.0"
                        },
-                       streams=[some_application_cpu_usage],
-                       checks=[cpu_max_average_check])
+                       streams=[some_application_2xx_responses, some_application_5xx_responses],
+                       checks=[max_response_ratio_check, max_percentile_response_check, failed_response_ratio_check,
+                               min_percentile_response_check])
 
         self.relation("urn:example/application:some_application", "urn:example/host:this_host", "IS_HOSTED_ON", {})
 
-        self.gauge("host.cpu.usage", randint(0, 100), tags=["hostname:this-host"])
+        self.gauge("2xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("2xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("2xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("2xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("5xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("5xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("5xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
+        self.gauge("5xx.responses", randint(0, 100), tags=["application:some_application", "region:eu-west-1"])
 
         self.event({
             "timestamp": int(time.time()),
--- agent_integration_sample/tests/test_agent_integration_sample.py
+++ agent_integration_sample/tests/test_agent_integration_sample.py
@@ -42,9 +42,300 @@ class TestAgentIntegration(unittest.TestCase):
         # TODO this is needed because the topology retains data across tests
         topology.reset()
 
-        result = self.check.run()
-        print(result)
+        self.check.run()
         topo_instances = topology.get_snapshot(self.check.check_id)
-        print(topo_instances)
-        self.assertEqual(len(topo_instances['components']), 4)
-        self.assertEqual(len(topo_instances['relations']), 2)
+        self.assertEqual(len(topo_instances['components']), 5)
+        self.assertEqual(len(topo_instances['relations']), 3)
+
+        assert topo_instances == {
+          'components': [
+            {
+              'data': {
+                'cluster': 'stubbed-cluster-name',
+                'hostname': 'stubbed.hostname',
+                'identifiers': [
+                  'urn:process/:stubbed.hostname:1:1234567890'
+                ],
+                'name': 'StackState Agent:stubbed.hostname',
+                'tags': [
+                  'hostname:stubbed.hostname',
+                  'stackstate-agent'
+                ]
+              },
+              'id': 'urn:stackstate-agent/:stubbed.hostname',
+              'type': 'stackstate-agent'
+            },
+            {
+              'data': {
+                'checks': [
+                  {
+                    'is_service_check_health_check': True,
+                    'name': 'Integration Health',
+                    'stream_id': -1
+                  }
+                ],
+                'cluster': 'stubbed-cluster-name',
+                'events': [
+                  {
+                    'conditions': [
+                      {
+                        'key': 'hostname',
+                        'value': 'stubbed.hostname'
+                      },
+                      {
+                        'key': 'integration-type',
+                        'value': 'agent-integration'
+                      }
+                    ],
+                    'identifier': topo_instances['components'][1]['data']['events'][0]['identifier'],
+                    'name': 'Service Checks',
+                    'stream_id': -1
+                  }
+                ],
+                'hostname': 'stubbed.hostname',
+                'integration': 'agent-integration',
+                'name': 'stubbed.hostname:agent-integration',
+                'tags': [
+                  'hostname:stubbed.hostname',
+                  'agent-integration:agent-integration'
+                ]
+              },
+              'id': 'urn:agent-integration/:stubbed.hostname:agent-integration',
+              'type': 'agent-integration'
+            },
+            {
+              'data': {
+                'checks': [
+                  {
+                    'is_service_check_health_check': True,
+                    'name': 'Integration Instance Health',
+                    'stream_id': -1
+                  }
+                ],
+                'cluster': 'stubbed-cluster-name',
+                'events': [
+                  {
+                    'conditions': [
+                      {
+                        'key': 'hostname',
+                        'value': 'stubbed.hostname'
+                      },
+                      {
+                        'key': 'integration-type',
+                        'value': 'agent-integration'
+                      },
+                      {
+                        'key': 'integration-url',
+                        'value': 'sample'
+                      }
+                    ],
+                    'identifier': topo_instances['components'][2]['data']['events'][0]['identifier'],
+                    'name': 'Service Checks',
+                    'stream_id': -1
+                  }
+                ],
+                'hostname': 'stubbed.hostname',
+                'integration': 'agent-integration',
+                'name': 'agent-integration:sample',
+                'tags': [
+                  'hostname:stubbed.hostname',
+                  'agent-integration:agent-integration',
+                  'agent-integration-url:sample'
+                ]
+              },
+              'id': 'urn:agent-integration-instance/:stubbed.hostname:agent-integration:sample',
+              'type': 'agent-integration-instance'
+            },
+            {
+              'data': {
+                'checks': [
+                  {
+                    'critical_value': 90,
+                    'deviating_value': 75,
+                    'is_metric_maximum_average_check': True,
+                    'max_window': 300000,
+                    'name': 'Max CPU Usage (Average)',
+                    'stream_id': -1
+                  },
+                  {
+                    'critical_value': 90,
+                    'deviating_value': 75,
+                    'is_metric_maximum_last_check': True,
+                    'max_window': 300000,
+                    'name': 'Max CPU Usage (Last)',
+                    'stream_id': -1
+                  },
+                  {
+                    'critical_value': 5,
+                    'deviating_value': 10,
+                    'is_metric_minimum_average_check': True,
+                    'max_window': 300000,
+                    'name': 'Min CPU Usage (Average)',
+                    'stream_id': -1
+                  },
+                  {
+                    'critical_value': 5,
+                    'deviating_value': 10,
+                    'is_metric_minimum_last_check': True,
+                    'max_window': 300000,
+                    'name': 'Min CPU Usage (Last)',
+                    'stream_id': -1
+                  }
+                ],
+                'domain': 'Webshop',
+                'environment': 'Production',
+                'identifiers': [
+                  'another_identifier_for_this_host'
+                ],
+                'labels': [
+                  'host:this_host',
+                  'region:eu-west-1'
+                ],
+                'layer': 'Hosts',
+                'metrics': [
+                  {
+                    'aggregation': 'MEAN',
+                    'conditions': [
+                      {
+                        'key': 'hostname',
+                        'value': 'this-host'
+                      }
+                    ],
+                    'identifier': topo_instances['components'][3]['data']['metrics'][0]['identifier'],
+                    'metric_field': 'host.cpu.usage',
+                    'name': 'Host CPU Usage',
+                    'priority': 'HIGH',
+                    'stream_id': -1,
+                    'unit_of_measure': 'Percentage'
+                  }
+                ],
+                'name': 'this-host'
+              },
+              'id': 'urn:example/host:this_host',
+              'type': 'Host'
+            },
+            {
+              'data': {
+                'checks': [
+                  {
+                    'critical_value': 75,
+                    'denominator_stream_id': -1,
+                    'deviating_value': 50,
+                    'is_metric_maximum_ratio_check': True,
+                    'max_window': 300000,
+                    'name': 'OK vs Error Responses',
+                    'numerator_stream_id': -2
+                  },
+                  {
+                    'critical_value': 70,
+                    'deviating_value': 50,
+                    'is_metric_maximum_percentile_check': True,
+                    'max_window': 300000,
+                    'name': 'Error Response 99th Percentile',
+                    'percentile': 99,
+                    'stream_id': -2
+                  },
+                  {
+                    'critical_value': 75,
+                    'denominator_stream_id': -1,
+                    'deviating_value': 50,
+                    'is_metric_failed_ratio_check': True,
+                    'max_window': 300000,
+                    'name': 'OK vs Error Responses',
+                    'numerator_stream_id': -2
+                  },
+                  {
+                    'critical_value': 5,
+                    'deviating_value': 10,
+                    'is_metric_minimum_percentile_check': True,
+                    'max_window': 300000,
+                    'name': 'Success Response 99th Percentile',
+                    'percentile': 99,
+                    'stream_id': -1
+                  }
+                ],
+                'domain': 'Webshop',
+                'environment': 'Production',
+                'identifiers': [
+                  'another_identifier_for_some_application'
+                ],
+                'labels': [
+                  'application:some_application',
+                  'region:eu-west-1',
+                  'hosted_on:this-host'
+                ],
+                'layer': 'Applications',
+                'metrics': [
+                  {
+                    'aggregation': 'MEAN',
+                    'conditions': [
+                      {
+                        'key': 'application',
+                        'value': 'some_application'
+                      },
+                      {
+                        'key': 'region',
+                        'value': 'eu-west-1'
+                      }
+                    ],
+                    'identifier': topo_instances['components'][4]['data']['metrics'][0]['identifier'],
+                    'metric_field': '2xx.responses',
+                    'name': '2xx Responses',
+                    'priority': 'HIGH',
+                    'stream_id': -1,
+                    'unit_of_measure': 'Count'
+                  },
+                  {
+                    'aggregation': 'MEAN',
+                    'conditions': [
+                      {
+                        'key': 'application',
+                        'value': 'some_application'
+                      },
+                      {
+                        'key': 'region',
+                        'value': 'eu-west-1'
+                      }
+                    ],
+                    'identifier': topo_instances['components'][4]['data']['metrics'][1]['identifier'],
+                    'metric_field': '5xx.responses',
+                    'name': '5xx Responses',
+                    'priority': 'HIGH',
+                    'stream_id': -2,
+                    'unit_of_measure': 'Count'
+                  }
+                ],
+                'name': 'some-application',
+                'version': '0.2.0'
+              },
+              'id': 'urn:example/application:some_application',
+              'type': 'Application'
+            }
+          ],
+          'instance_key': {
+            'type': 'agent',
+            'url': 'integrations'
+          },
+          'relations': [
+            {
+              'data': {},
+              'source_id': 'urn:stackstate-agent/:stubbed.hostname',
+              'target_id': 'urn:agent-integration/:stubbed.hostname:agent-integration',
+              'type': 'runs'
+            },
+            {
+              'data': {},
+              'source_id': 'urn:agent-integration/:stubbed.hostname:agent-integration',
+              'target_id': 'urn:agent-integration-instance/:stubbed.hostname:agent-integration:sample',
+              'type': 'has'
+            },
+            {
+              'data': {},
+              'source_id': 'urn:example/application:some_application',
+              'target_id': 'urn:example/host:this_host',
+              'type': 'IS_HOSTED_ON'
+            }
+          ],
+          'start_snapshot': False,
+          'stop_snapshot': False
+        }
