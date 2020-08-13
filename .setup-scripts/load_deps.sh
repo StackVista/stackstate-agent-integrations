cac51a4b0ce46a438f53cb946110ab0eab7bf21f
vishal kumar <vishal.kumar@xebia.com>
2020-08-13 23:09:37 +0530

    try one more

--- .setup-scripts/load_deps.sh
+++ .setup-scripts/load_deps.sh
@@ -2,7 +2,6 @@
 
 set -x
 
-pip install -U setuptools codecov
+pip install --disable-pip-version-check -U pip setuptools codecov
 pip install "./stackstate_checks_dev[cli]"
-pip install pip==19.3.1
 set +x
