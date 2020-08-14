7ba2617bf43cf0922a4db96a74142ae6d451ead8
vishal kumar <vishal.kumar@xebia.com>
2020-08-14 13:29:53 +0530

    pin pip in tox

--- .setup-scripts/load_deps.sh
+++ .setup-scripts/load_deps.sh
@@ -2,6 +2,7 @@
 
 set -x
 
-pip install --disable-pip-version-check -U pip setuptools codecov
+pip install -U pip setuptools codecov
 pip install "./stackstate_checks_dev[cli]"
+pip install tox-pip-version
 set +x
