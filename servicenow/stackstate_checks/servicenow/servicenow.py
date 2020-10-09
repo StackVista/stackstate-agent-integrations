80f5c101240d26ca0ba20578bcafe7f2b2e53f3f
vishal kumar <vishal.kumar@xebia.com>
2020-10-09 16:20:59 +0530

    type in with_snapshots param

--- servicenow/stackstate_checks/servicenow/servicenow.py
+++ servicenow/stackstate_checks/servicenow/servicenow.py
@@ -33,7 +33,7 @@ class ServicenowCheck(AgentCheck):
         if "url" not in instance:
             raise ConfigurationError("Missing 'url' in instance configuration.")
 
-        return TopologyInstance(self.INSTANCE_TYPE, instance["url"], with_snapshot=False)
+        return TopologyInstance(self.INSTANCE_TYPE, instance["url"], with_snapshots=False)
 
     def check(self, instance):
         if 'url' not in instance:
