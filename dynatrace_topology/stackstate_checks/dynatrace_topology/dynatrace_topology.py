7ad025397d9019dff119b011d39ef85c9f0a1132
vishal kumar <vishal.kumar@xebia.com>
2020-10-16 20:14:56 +0530

    dynatrace topology integration, get config from core agent and duplicate topology removal in base

---
+++ dynatrace_topology/stackstate_checks/dynatrace_topology/dynatrace_topology.py
@@ -0,0 +1,277 @@
+# (C) StackState 2020
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from stackstate_checks.base import AgentCheck, ConfigurationError, TopologyInstance
+from .dynatrace_exception import DynatraceError
+
+import requests
+from requests import Session
+import yaml
+from datetime import datetime
+
+
+class DynatraceTopologyCheck(AgentCheck):
+
+    INSTANCE_TYPE = "dynatrace"
+    SERVICE_CHECK_NAME = "dynatrace"
+
+    def __init__(self, name, init_config, instances=None):
+        AgentCheck.__init__(self, name, init_config, instances)
+        self.url = None
+        self.token = None
+        self.tags = []
+        self.environment = None
+        self.domain = None
+        self.verify = None
+        self.cert = None
+        self.keyfile = None
+
+    def get_instance_key(self, instance):
+        if 'url' not in instance:
+            raise ConfigurationError('Missing url in configuration.')
+
+        return TopologyInstance(self.INSTANCE_TYPE, instance["url"], with_snapshots=False)
+
+    def check(self, instance):
+        """
+        Integration logic
+        """
+        if 'url' not in instance:
+            raise ConfigurationError('Missing URL in configuration.')
+        if 'token' not in instance:
+            raise ConfigurationError('Missing API Token in configuration.')
+
+        self.url = instance.get('url')
+        self.token = instance.get('token')
+        self.tags = instance.get('tags', [])
+        self.domain = instance.get('domain', '')
+        self.environment = instance.get('environment', 'production')
+        self.verify = instance.get('verify', True)
+        self.cert = instance.get('cert', '')
+        self.keyfile = instance.get('keyfile', '')
+
+        try:
+            self.start_snapshot()
+            self.process_topology()
+            self.stop_snapshot()
+            msg = "Dynatrace topology processed successfully"
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK, tags=self.tags, message=msg)
+        except Exception as e:
+            self.log.exception(str(e))
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL, tags=self.tags, message=str(e))
+
+    def process_topology(self):
+        """
+        Collects components and relations for each component type from dynatrace smartscape topology API
+        """
+        start_time = datetime.now()
+        self.log.info("Starting the collection of topology")
+        services = self.collect_services()
+        processes = self.collect_processes()
+        processes_groups = self.collect_process_groups()
+        hosts = self.collect_hosts()
+        applications = self.collect_applications()
+        topology = {"service": services, "process": processes, "process-group": processes_groups, "host": hosts,
+                    "application": applications}
+        # process component for each type
+        for comp_type, response in topology.items():
+            self.process_component(response, comp_type)
+        end_time = datetime.now()
+        time_taken = end_time - start_time
+        self.log.info("Time taken to collect the topology is: {} seconds".format(time_taken.total_seconds()))
+
+    def collect_relations(self, component, externalId):
+        """
+        Collects relationships from different component-types
+        :param component: the component for which relationships need to be extracted and processed
+        :param externalId: the component externalId for and from relationship will be created
+        :return: None
+        """
+        outgoing_relations = component.get("fromRelationships", {})
+        incoming_relations = component.get("toRelationships", {})
+        for relation_type in outgoing_relations.keys():
+            # Ignore `isSiteOf` relation since location components are not processed right now
+            if relation_type != "isSiteOf":
+                for target_id in outgoing_relations[relation_type]:
+                    self.relation(externalId, target_id, relation_type, {})
+        for relation_type in incoming_relations.keys():
+            # Ignore `isSiteOf` relation since location components are not processed right now
+            if relation_type != "isSiteOf":
+                for source_id in incoming_relations[relation_type]:
+                    self.relation(source_id, externalId, relation_type, {})
+
+    def filter_data(self, data):
+        """
+        Delete the un-necessary relationships from the data
+        """
+        if "fromRelationships" in data:
+            del data["fromRelationships"]
+        if "toRelationships" in data:
+            del data["toRelationships"]
+        return data
+
+    def collect_processes(self):
+        """
+        Collects the response from the Dynatrace Process API endpoint
+        """
+        endpoint = self.url + "/api/v1/entity/infrastructure/processes"
+        processes = self.get_json_response(endpoint)
+        return processes
+
+    def collect_hosts(self):
+        """
+        Collects the response from the Dynatrace Host API endpoint
+        """
+        endpoint = self.url + "/api/v1/entity/infrastructure/hosts"
+        hosts = self.get_json_response(endpoint)
+        return hosts
+
+    def collect_applications(self):
+        """
+        Collects the response from the Dynatrace Application API endpoint
+        """
+        endpoint = self.url + "/api/v1/entity/applications"
+        applications = self.get_json_response(endpoint)
+        return applications
+
+    def collect_process_groups(self):
+        """
+        Collects the response from the Dynatrace Process-Group API endpoint
+        """
+        endpoint = self.url + "/api/v1/entity/infrastructure/process-groups"
+        process_groups = self.get_json_response(endpoint)
+        return process_groups
+
+    def collect_services(self):
+        """
+        Collects the response from the Dynatrace Service API endpoint
+        """
+        endpoint = self.url + "/api/v1/entity/services"
+        services = self.get_json_response(endpoint)
+        return services
+
+    def process_component(self, response, component_type):
+        """
+        Process each component type and map those with specific data
+        :param response: Response of each component type endpoint
+        :param component_type: Component type
+        :return: create the component on stackstate API
+        """
+        urn = ''
+        if "error" not in response:
+            for item in response:
+                # special case for host type as we get some float values
+                if component_type == "host":
+                    for key in item.keys():
+                        if type(item[key]) is float:
+                            item[key] = str(item[key])
+                identifiers = []
+                data = dict()
+                externalId = item.get("entityId")
+                if component_type == "service":
+                    urn = "urn:service:/{}".format(externalId)
+                elif component_type == "process-group":
+                    urn = "urn:process-group:/{}".format(externalId)
+                elif component_type == "application":
+                    urn = "urn:application:/{}".format(externalId)
+                elif component_type == "process":
+                    urn = "urn:process:/{}".format(externalId)
+                elif component_type == "host":
+                    displayName = item.get("displayName")
+                    urn = "urn:host:/{}".format(displayName)
+                identifiers.append(urn)
+                # derive useful labels and get labels from dynatrace tags
+                labels = self.get_labels(item)
+                labels += self.get_labels_from_dynatrace_tags(item)
+                if "tags" in item:
+                    del item["tags"]
+                # prefix the labels with `dynatrace-` for all labels
+                labels = ["dynatrace-{}".format(label) for label in labels]
+                data.update(item)
+                data.update({
+                    "identifiers": identifiers,
+                    "tags": self.tags,
+                    "domain": self.domain,
+                    "environment": self.environment,
+                    "instance": self.url,
+                    "labels": labels
+                })
+                data = self.filter_data(data)
+                self.component(externalId, component_type, data)
+                self.collect_relations(item, externalId)
+        else:
+            self.log.warning("Problem getting the {0} or No {1} found.".format(component_type, component_type))
+            raise DynatraceError(response["error"].get("message"), response["error"].get("code"), component_type)
+
+    def get_labels_from_dynatrace_tags(self, item):
+        """
+        Process each tag as a label in component
+        :param item: the component item to read from
+        :return: list of added tags as labels
+        """
+        tags = []
+        for tag in item.get("tags", []):
+            tag_label = ''
+            if bool(tag['context']) and tag.get('context') != 'CONTEXTLESS':
+                tag_label += '[{0}]'.format(tag['context'])
+            if bool(tag.get('key')):
+                tag_label += "{0}".format(tag['key'])
+            if bool(tag.get('value')):
+                tag_label += ":{0}".format(tag['value'])
+            tags.append(tag_label)
+        return tags
+
+    def get_labels(self, item):
+        """
+        get labels for each component
+        :param item: the component item
+        :return: the list of added labels for a component
+        """
+        labels = []
+        # append management zones in labels for each existing component
+        if "managementZones" in item:
+            for zone in item["managementZones"]:
+                labels.append("managementZones:{}".format(zone.get("name")))
+        if "entityId" in item:
+            labels.append(item["entityId"])
+        if "monitoringState" in item:
+            actual_state = item["monitoringState"].get("actualMonitoringState")
+            expected_state = item["monitoringState"].get("expectedMonitoringState")
+            labels.append("actualMonitoringState:{}".format(actual_state))
+            labels.append("expectedMonitoringState:{}".format(expected_state))
+        if "softwareTechnologies" in item:
+            for technologies in item["softwareTechnologies"]:
+                tech_label = ''
+                if bool(technologies['type']):
+                    tech_label += technologies['type']
+                if bool(technologies['edition']):
+                    tech_label += ":{}".format(technologies['edition'])
+                if bool(technologies['version']):
+                    tech_label += ":{}".format(technologies['version'])
+                labels.append(tech_label)
+        return labels
+
+    def get_json_response(self, endpoint, timeout=10):
+        """
+        Make a request to Dynatrace endpoint through session
+        :param endpoint: Dynatrace API Endpoint to call
+        :param timeout: timeout for a response
+        :return: Response of each endpoint
+        """
+        headers = {"Authorization": "Api-Token {}".format(self.token)}
+        resp = None
+        msg = None
+        self.log.info("URL is {}".format(endpoint))
+        try:
+            session = Session()
+            session.headers.update(headers)
+            session.verify = self.verify
+            if self.cert:
+                session.cert = (self.cert, self.keyfile)
+            resp = session.get(endpoint)
+            if resp.status_code != 200:
+                raise Exception("Got %s when hitting %s" % (resp.status_code, endpoint))
+        except requests.exceptions.Timeout:
+            msg = "{} seconds timeout when hitting {}".format(timeout, endpoint)
+            raise Exception("Exception occured for endpoint {0} with message: {1}".format(endpoint, msg))
+        return yaml.safe_load(resp.text)
