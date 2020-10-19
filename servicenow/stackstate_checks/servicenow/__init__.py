605572da0cadad69e026ba8946fa5eb09da589fb
vishal <39790094+vishalxebia@users.noreply.github.com>
2020-10-19 16:33:36 +0530

    Stac 10855 servicenow bug (#63)
    * handle corner exception and key
    
    * snapshot false in case of error
    
    * type in with_snapshots param
    
    * filter the data first
    
    * stop snapshot only after success
    
    * implement batching for components and handle corner cases exception
    
    * test imports InstanceInfo
    
    * uniform url
    
    * mandatory instance values
    
    Co-authored-by: Hrvoje Ruhek <hrvoje.ruhek@ag04.com>

--- servicenow/stackstate_checks/servicenow/__init__.py
+++ servicenow/stackstate_checks/servicenow/__init__.py
@@ -2,9 +2,10 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .servicenow import ServicenowCheck
+from .servicenow import ServicenowCheck, InstanceInfo
 
 __all__ = [
     '__version__',
-    'ServicenowCheck'
+    'ServicenowCheck',
+    'InstanceInfo'
 ]
