95840f34698dba5513601cca06c1347d0be8d56a
vishal kumar <vishal.kumar@xebia.com>
2020-11-04 14:13:16 +0530

    reconnect on unauntheticated exception

--- vsphere/stackstate_checks/vsphere/vsphere.py
+++ vsphere/stackstate_checks/vsphere/vsphere.py
@@ -378,6 +378,7 @@ class VSphereCheck(AgentCheck):
 
         # Connections open to vCenter instances
         self.server_instances = {}
+        self.session = None
 
         # Event configuration
         self.event_config = {}
@@ -488,14 +489,7 @@ class VSphereCheck(AgentCheck):
         now = time.time()
         return now - self.cache_times[i_key][entity][LAST] > self.cache_times[i_key][entity][INTERVAL]
 
-    def _get_server_instance(self, instance):
-        i_key = self._instance_key(instance)
-
-        service_check_tags = [
-            'vcenter_server:{0}'.format(instance.get('name')),
-            'vcenter_host:{0}'.format(instance.get('host')),
-        ]
-
+    def _smart_connect(self, instance, service_check_tags):
         # Check for ssl configs and generate an appropriate ssl context object
         ssl_verify = instance.get('ssl_verify', True)
         ssl_capath = instance.get('ssl_capath', None)
@@ -509,38 +503,57 @@ class VSphereCheck(AgentCheck):
 
         # If both configs are used, log a message explaining the default
         if not ssl_verify and ssl_capath:
-            self.log.debug("Your configuration is incorrectly attempting to "
-                           "specify both a CA path, and to disable SSL "
-                           "verification. You cannot do both. Proceeding with "
-                           "disabling ssl verification.")
+            self.log.debug(
+                "Your configuration is incorrectly attempting to "
+                "specify both a CA path, and to disable SSL "
+                "verification. You cannot do both. Proceeding with "
+                "disabling ssl verification."
+            )
+
+        try:
+            # Object returned by SmartConnect is a ServerInstance
+            # https://www.vmware.com/support/developer/vc-sdk/visdk2xpubs/ReferenceGuide/vim.ServiceInstance.html
+            server_instance = connect.SmartConnect(
+                host=instance.get('host'),
+                user=instance.get('username'),
+                pwd=instance.get('password'),
+                sslContext=context if not ssl_verify or ssl_capath else None,
+            )
+        except Exception as e:
+            err_msg = "Connection to {} failed: {}".format(instance.get('host'), e)
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL, tags=service_check_tags, message=err_msg)
+            raise Exception(err_msg)
+
+        # Check that we have sufficient permission for the calls we need to make
+        try:
+            server_instance.CurrentTime()
+        except Exception as e:
+            err_msg = (
+                "A connection to {} can be established, but performing operations on the server fails: {}"
+            ).format(instance.get('host'), e)
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL, tags=service_check_tags, message=err_msg)
+            raise Exception(err_msg)
+
+        return server_instance
+
+    def _get_server_instance(self, instance):
+        i_key = self._instance_key(instance)
+
+        service_check_tags = [
+            'vcenter_server:{0}'.format(instance.get('name')),
+            'vcenter_host:{0}'.format(instance.get('host')),
+        ]
 
         if i_key not in self.server_instances:
-            try:
-                # Object returned by SmartConnect is a ServerInstance
-                #   https://www.vmware.com/support/developer/vc-sdk/visdk2xpubs/ReferenceGuide/vim.ServiceInstance.html
-                server_instance = connect.SmartConnect(
-                    host=instance.get('host'),
-                    user=instance.get('username'),
-                    pwd=instance.get('password'),
-                    connectionPoolTimeout=-1,
-                    sslContext=context if not ssl_verify or ssl_capath else None
-                )
-            except Exception as e:
-                err_msg = "Connection to %s failed: %s" % (instance.get('host'), e)
-                self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
-                                   tags=service_check_tags, message=err_msg)
-                raise Exception(err_msg)
-            self.server_instances[i_key] = server_instance
+            self.server_instances[i_key] = self._smart_connect(instance, service_check_tags)
 
         # Test if the connection is working
         try:
-            self.server_instances[i_key].RetrieveContent()
+            self.server_instances[i_key].CurrentTime()
             self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK, tags=service_check_tags)
         except Exception as e:
-            err_msg = "Connection to %s died unexpectedly: %s" % (instance.get('host'), e)
-            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
-                               tags=service_check_tags, message=err_msg)
-            raise Exception(err_msg)
+            # Try to reconnect, if the connection is definitely broken.
+            self.server_instances[i_key] = self._smart_connect(instance, service_check_tags)
         return self.server_instances[i_key]
 
     def _compute_needed_metrics(self, instance, available_metrics):
@@ -1251,8 +1264,8 @@ class VSphereCheck(AgentCheck):
         return obj_list
 
     def vsphere_client_connect(self, instance):
-        session = requests.session()
-        session.verify = False
+        self.session = requests.session()
+        self.session.verify = False
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
         # Connect to vSphere client
@@ -1260,7 +1273,7 @@ class VSphereCheck(AgentCheck):
             server=instance.get('host'),
             username=instance.get('username'),
             password=instance.get('password'),
-            session=session)
+            session=self.session)
 
     def get_topologyitems_sync(self, instance):
         server_instance = self._get_server_instance(instance)
@@ -1281,6 +1294,10 @@ class VSphereCheck(AgentCheck):
         clustercomputeresources = yaml.safe_load(json.dumps(self._vsphere_clustercomputeresources(content, domain,
                                                                                                   regexes)))
         computeresource = yaml.safe_load(json.dumps(self._vsphere_computeresources(content, domain, regexes)))
+
+        # close the session after the processing of all Tags from the new Rest Client
+        if self.session:
+            self.session.close()
 
         return {
             "vms": vms,
