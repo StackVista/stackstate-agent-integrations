65fdfc1204877aae5da19069980a376ab9461298
zandrewitte <zandre.witte@gmail.com>
2020-11-10 10:07:56 +0100

    add persistent state for integrations

--- stackstate_checks_base/tests/test_utils.py
+++ stackstate_checks_base/tests/test_utils.py
@@ -3,8 +3,15 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from decimal import ROUND_HALF_DOWN, ROUND_HALF_UP
 
+import pytest
+import os
 from stackstate_checks.utils.common import pattern_filter, round_value
 from stackstate_checks.utils.limiter import Limiter
+from stackstate_checks.utils.persistent_state import PersistentState, PersistentInstance, StateNotPersistedException, \
+    StateCorruptedException, StateReadException
+from six import PY3
+from schematics.models import Model
+from schematics.types import IntType
 
 
 class Item:
@@ -119,3 +126,47 @@ class TestRounding():
         assert round_value(2.555, precision=2) == 2.560
         assert round_value(4.2345, precision=2) == 4.23
         assert round_value(4.2345, precision=3) == 4.235
+
+
+class TestStorageSchema(Model):
+    offset = IntType(required=True, default=0)
+
+
+class TestPersistentState:
+
+    def test_exception_state_without_valid_location(self, state):
+        s = {'a': 'b', 'c': 1, 'd': ['e', 'f', 'g'], 'h': {'i': 'j', 'k': True}}
+        instance = PersistentInstance("state.without.location", "")
+        with pytest.raises(StateReadException) as e:
+            state.persistent_state.get_state(instance)
+        assert str(e.value) == """[Errno 2] No such file or directory: ''"""
+
+        with pytest.raises(StateNotPersistedException) as e:
+            state.persistent_state.set_state(instance, s)
+        assert str(e.value) == """[Errno 2] No such file or directory: ''"""
+
+    def test_exception_corrupted_state(self, state):
+        instance = PersistentInstance("state.with.corrupted.data", "state.with.corrupted.data")
+        # write "corrupted" data
+        with open(instance.file_location, 'w') as f:
+            f.write("{'a': 'b', 'c': 1, 'd':....")
+
+        with pytest.raises(StateCorruptedException) as e:
+            state.persistent_state.get_state(instance)
+        if PY3:
+            assert str(e.value) == """Expecting property name enclosed in double quotes: line 1 column 2 (char 1)"""
+        else:
+            assert str(e.value) == """Expecting property name: line 1 column 2 (char 1)"""
+
+        os.remove(instance.file_location)
+
+    def test_state_flushing(self, state):
+        s = {'a': 'b', 'c': 1, 'd': ['e', 'f', 'g'], 'h': {'i': 'j', 'k': True}}
+        instance = PersistentInstance("on.disk.state", "on.disk.state")
+        state.assert_state(instance, s)
+
+    def test_state_flushing_with_schema(self, state):
+        s = TestStorageSchema({'offset': 10})
+        instance = PersistentInstance("on.disk.state.schema", "on.disk.state.schema")
+        rs = state.assert_state(instance, s, TestStorageSchema)
+        assert rs.offset == s.offset
