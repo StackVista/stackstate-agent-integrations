aa7d46779b6d622c84f11cf661c518b54ab13962
zandrewitte <zandre.witte@gmail.com>
2020-11-11 13:48:57 +0100

    line indent

--- stackstate_checks_base/stackstate_checks/base/checks/base.py
+++ stackstate_checks_base/stackstate_checks/base/checks/base.py
@@ -394,14 +394,14 @@ class AgentCheckBase(object):
         return data
 
     @deprecated(version='2.9.0', reason="Topology Snapshots is enabled by default for all TopologyInstance checks, "
-                                         "to disable snapshots use TopologyInstance(type, url, with_snapshots=False) "
-                                         "when overriding get_instance_key")
+                                        "to disable snapshots use TopologyInstance(type, url, with_snapshots=False) "
+                                        "when overriding get_instance_key")
     def start_snapshot(self):
         topology.submit_start_snapshot(self, self.check_id, self._get_instance_key())
 
     @deprecated(version='2.9.0', reason="Topology Snapshots is enabled by default for all TopologyInstance checks, "
-                                         "to disable snapshots use TopologyInstance(type, url, with_snapshots=False) "
-                                         "when overriding get_instance_key")
+                                        "to disable snapshots use TopologyInstance(type, url, with_snapshots=False) "
+                                        "when overriding get_instance_key")
     def stop_snapshot(self):
         topology.submit_stop_snapshot(self, self.check_id, self._get_instance_key())
 
