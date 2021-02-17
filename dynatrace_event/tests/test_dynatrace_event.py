1d38050882341ca1c29ce44052b5417f8eece5ae
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-02-17 09:43:21 +0100

    test are updated to reflect previous change

--- dynatrace_event/tests/test_dynatrace_event.py
+++ dynatrace_event/tests/test_dynatrace_event.py
@@ -9,7 +9,6 @@ import requests_mock
 
 from stackstate_checks.base import AgentCheck
 from stackstate_checks.base.stubs import aggregator, telemetry
-from stackstate_checks.dynatrace_event.dynatrace_event import generate_bootstrap_timestamp
 
 CHECK_NAME = 'dynatrace_event'
 
@@ -19,11 +18,10 @@ def test_check_for_empty_events(check, instance):
     Testing Dynatrace event check should not produce any events
     """
     with requests_mock.Mocker() as m:
-        url = '{}/api/v1/events?from={}'.format(instance['url'], generate_bootstrap_timestamp(5))
+        url = '{}/api/v1/events?from={}'.format(instance['url'], check.generate_bootstrap_timestamp(5))
         m.get(url, status_code=200, text=read_data_from_file('no_events.json'))
         check.run()
-        aggregator.assert_service_check(CHECK_NAME, count=1, status=AgentCheck.OK,
-                                        message='Dynatrace events processed successfully')
+        aggregator.assert_service_check(CHECK_NAME, count=1, status=AgentCheck.OK)
         assert len(aggregator.events) == 0
 
 
@@ -33,7 +31,7 @@ def test_check_for_event_limit_reached_condition(check, instance):
     between subsequent check runs exceed the `events_process_limit`
     """
     with requests_mock.Mocker() as m:
-        url = '{}/api/v1/events?from={}'.format(instance['url'], generate_bootstrap_timestamp(5))
+        url = '{}/api/v1/events?from={}'.format(instance['url'], check.generate_bootstrap_timestamp(5))
         m.get(url, status_code=200, text=read_data_from_file('21events.json'))
         check.run()
         aggregator.assert_service_check(CHECK_NAME, count=1, status=AgentCheck.CRITICAL,
@@ -45,7 +43,7 @@ def test_check_respects_events_process_limit_on_startup(check, instance):
     Testing Dynatrace should respect `events_process_limit` config and just produce those number of events
     """
     with requests_mock.Mocker() as m:
-        url1 = '{}/api/v1/events?from={}'.format(instance['url'], generate_bootstrap_timestamp(5))
+        url1 = '{}/api/v1/events?from={}'.format(instance['url'], check.generate_bootstrap_timestamp(5))
         url2 = '{}/api/v1/events?cursor={}'.format(instance['url'], '123')
         url3 = '{}/api/v1/events?cursor={}'.format(instance['url'], '345')
         m.get(url1, status_code=200, text=read_data_from_file("events_set1.json"))
@@ -61,7 +59,7 @@ def test_check_for_error_in_events(check):
     Testing Dynatrace check should produce service check when error thrown
     """
     error_msg = "mocked test error occurred"
-    check.get_dynatrace_event_json_response = mock.MagicMock(return_value={"error": {"message": error_msg}})
+    check._get_dynatrace_event_json_response = mock.MagicMock(return_value={"error": {"message": error_msg}})
     check.run()
     aggregator.assert_service_check(CHECK_NAME, count=1, status=AgentCheck.CRITICAL,
                                     message='Error in pulling the events: {}'.format(error_msg))
@@ -74,7 +72,7 @@ def test_check_raise_exception_for_response_code_not_200(check, instance):
     """
 
     with requests_mock.Mocker() as m:
-        url = '{}/api/v1/events?from={}'.format(instance['url'], generate_bootstrap_timestamp(5))
+        url = '{}/api/v1/events?from={}'.format(instance['url'], check.generate_bootstrap_timestamp(5))
         m.get(url, status_code=500, text='error')
         check.run()
         error_message = 'Got 500 when hitting https://instance.live.dynatrace.com/api/v1/events'
@@ -86,7 +84,7 @@ def test_check_raise_exception(check):
     """
     Test to raise a exception from code that talks to API endpoint throws exception
     """
-    check.get_dynatrace_event_json_response = mock.MagicMock(side_effect=Exception("Mocked exception occurred"))
+    check._get_dynatrace_event_json_response = mock.MagicMock(side_effect=Exception("Mocked exception occurred"))
     check.run()
     assert len(aggregator.events) == 0
     aggregator.assert_service_check(CHECK_NAME, count=1, status=AgentCheck.CRITICAL,
@@ -97,7 +95,7 @@ def test_check_for_generated_events(check, instance):
     """
     Testing Dynatrace check should produce full events
     """
-    empty_state_timestamp = generate_bootstrap_timestamp(5)
+    empty_state_timestamp = check.generate_bootstrap_timestamp(5)
     with requests_mock.Mocker() as m:
         url = '{}/api/v1/events?from={}'.format(instance['url'], empty_state_timestamp)
         m.get(url, status_code=200, text=read_data_from_file('9events.json'))
