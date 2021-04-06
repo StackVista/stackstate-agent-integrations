d564795ee6662c6a614050b6847af96559f8b7d0
Jacco Kulman <jaccokulman@binx.io>
2021-04-06 19:48:14 +0200

    firehose missing

--- aws_topology/stackstate_checks/aws_topology/resources/__init__.py
+++ aws_topology/stackstate_checks/aws_topology/resources/__init__.py
@@ -6,6 +6,7 @@ from .ec2_vpc import process_vpcs
 from .ec2_vpn_gateway import process_vpn_gateways
 from .security_group import process_security_group
 from .sns import process_sns
+from .firehose import process_firehose
 
 __all__ = [
     'process_s3',
@@ -15,5 +16,6 @@ __all__ = [
     'process_vpcs',
     'process_vpn_gateways',
     'process_security_group',
-    'process_sns'
+    'process_sns',
+    'process_firehose'
 ]
