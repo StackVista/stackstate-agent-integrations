1afc18bc1ad828ccbc6562d53f5be6019497b215
Jacco Kulman <jaccokulman@binx.io>
2021-04-10 10:27:11 +0200

    added dynamodb

---
+++ aws_topology/stackstate_checks/aws_topology/resources/dynamodb.py
@@ -0,0 +1,40 @@
+from ..utils import make_valid_data, with_dimensions
+from .registry import RegisteredResource
+
+
+class dynamodb(RegisteredResource):
+    API = "dynamodb"
+    COMPONENT_TYPE = "aws.dynamodb"
+
+    def process_all(self):
+        dynamodb = {}
+        for page in self.client.get_paginator('list_tables').paginate():
+            for table_name in page.get('TableNames') or []:
+                result = self.process_table(table_name)
+                dynamodb.update(result)
+        return dynamodb
+
+    def process_table(self, table_name):
+        table_description_raw = self.client.describe_table(TableName=table_name)
+        table_description = make_valid_data(table_description_raw)
+        table_data = table_description['Table']
+        table_arn = table_data['TableArn']
+        table_tags = self.client.list_tags_of_resource(ResourceArn=table_arn).get('Tags') or []
+        table_data['Tags'] = table_tags
+        table_data['Name'] = table_arn
+        table_data.update(with_dimensions([{'key': 'TableName', 'value': table_name}]))
+        self.agent.component(table_arn, self.COMPONENT_TYPE, table_data)
+        latest_stream_arn = table_data.get('LatestStreamArn')
+        if latest_stream_arn and table_data.get('StreamSpecification'):
+            stream_specification = table_data['StreamSpecification']
+            latest_stream_label = table_data['LatestStreamLabel']
+            stream_specification['LatestStreamArn'] = latest_stream_arn
+            stream_specification['LatestStreamLabel'] = latest_stream_label
+            stream_specification['Name'] = latest_stream_arn
+            stream_specification.update(with_dimensions([
+                {'key': 'TableName', 'value': table_name},
+                {'key': 'StreamLabel', 'value': latest_stream_label}
+            ]))
+            self.agent.component(latest_stream_arn, 'aws.dynamodb.streams', stream_specification)
+            self.agent.relation(table_arn, latest_stream_arn, 'uses service', {})
+        return {table_name: table_arn}
