025dddb4a583e906adcaea544e7f537e116a2b4b
Jacco Kulman <jaccokulman@binx.io>
2021-04-11 20:00:41 +0200

    remove sleep(2) kinesis

--- aws_topology/stackstate_checks/aws_topology/resources/kinesis.py
+++ aws_topology/stackstate_checks/aws_topology/resources/kinesis.py
@@ -1,4 +1,3 @@
-import time
 from ..utils import make_valid_data
 from .registry import RegisteredResource
 
@@ -10,17 +9,16 @@ class kinesis(RegisteredResource):
     def process_all(self):
         kinesis_stream = {}
         for list_streams_page in self.client.get_paginator('list_streams').paginate():
-            for stream_name_raw in list_streams_page.get('StreamNames') or []:
-                stream_name = make_valid_data(stream_name_raw)
+            for stream_name in list_streams_page.get('StreamNames') or []:
                 result = self.process_stream(stream_name)
                 kinesis_stream.update(result)
         return kinesis_stream
 
     def process_stream(self, stream_name):
-        stream_summary = self.client.describe_stream_summary(StreamName=stream_name)
+        stream_summary_raw = self.client.describe_stream_summary(StreamName=stream_name)
+        stream_summary = make_valid_data(stream_summary_raw)
         stream_tags = self.client.list_tags_for_stream(StreamName=stream_name).get('Tags') or []
         stream_summary['Tags'] = stream_tags
         stream_arn = stream_summary['StreamDescriptionSummary']['StreamARN']
         self.agent.component(stream_arn, self.COMPONENT_TYPE, stream_summary)
-        time.sleep(2)
         return {stream_name: stream_arn}
