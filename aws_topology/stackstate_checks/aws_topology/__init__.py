6ab6ab111759b243940daad11f534096ababe0f4
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-04-20 16:41:49 +0200

    Fixed resource namespace error

--- aws_topology/stackstate_checks/aws_topology/__init__.py
+++ aws_topology/stackstate_checks/aws_topology/__init__.py
@@ -2,12 +2,11 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .aws_topology import AwsTopologyCheck, AwsClient, InstanceInfo, State
+from .aws_topology import AwsTopologyCheck, AwsClient, InstanceInfo
 
 __all__ = [
     '__version__',
     'AwsTopologyCheck',
     'AwsClient',
     'InstanceInfo',
-    'State'
 ]
