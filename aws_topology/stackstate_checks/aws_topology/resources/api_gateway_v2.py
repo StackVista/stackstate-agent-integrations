2c22ea1d3864fbdf6bffbc6db6db81a1cca05466
Jacco Kulman <jaccokulman@binx.io>
2021-05-15 00:29:25 +0200

    apigatewayv2 refactored

--- aws_topology/stackstate_checks/aws_topology/resources/api_gateway_v2.py
+++ aws_topology/stackstate_checks/aws_topology/resources/api_gateway_v2.py
@@ -1,6 +1,8 @@
-from .utils import make_valid_data, create_arn as arn
+from .utils import make_valid_data, create_arn as arn, client_array_operation
 from .registry import RegisteredResourceCollector
-
+from collections import namedtuple
+from schematics import Model
+from schematics.types import StringType
 
 def create_httpapi_arn(region=None, account_id=None, resource_id=None, **kwargs):
     return arn(resource='execute-api', region=region, account_id=account_id, resource_id=resource_id)
@@ -10,59 +12,113 @@ def create_httpapi_endpoint(api_id, region):
     return "https://{}.execute-api.{}.amazonaws.com".format(api_id, region)
 
 
+class Api(Model):
+    ApiId = StringType(required=True)
+
+
+class Stage(Model):
+    DeploymentId = StringType()
+    StageName = StringType(default="UNKNOWN")
+
+
+class Route(Model):
+    Target = StringType(default='')
+
+
+ApiData = namedtuple('ApiData', ['summary', 'stages', 'routes', 'integrations'])
+
+
 class ApigatewayV2Collector(RegisteredResourceCollector):
     API = "apigatewayv2"
     API_TYPE = "regional"
     COMPONENT_TYPE = "aws.httpapi"
 
+    def collect_stages(self, api_id):
+        return [stage for stage in client_array_operation(
+            self.client,
+            'get_stages',
+            'Items',
+            ApiId=api_id
+        )]
+
+    def collect_routes(self, api_id):
+        return [route for route in client_array_operation(
+            self.client,
+            'get_routes',
+            'Items',
+            ApiId=api_id
+        )]
+
+    def collect_integrations(self, api_id):
+        return {
+            integration.get('IntegrationId'): integration
+            for integration in client_array_operation(
+                self.client,
+                'get_integrations',
+                'Items',
+                ApiId=api_id
+            )
+        }
+
+    def collect_api(self, api_summary):
+        api_id = api_summary.get('ApiId')
+        stages = self.collect_stages(api_id) or []
+        routes = self.collect_routes(api_id) or []
+        integrations = self.collect_integrations(api_id) or []
+        return ApiData(summary=api_summary, stages=stages, routes=routes, integrations=integrations)
+
+    def collect_apis(self):
+        for api in [
+                self.collect_api(api_summary) for api_summary in client_array_operation(
+                    self.client,
+                    'get_apis',
+                    'Items'
+                )
+        ]:
+            yield api
+
     def process_all(self, filter=None):
-        try:
-            for apis_page in self.client.get_paginator('get_apis').paginate():
-                for api in apis_page.get('Items') or []:
-                    api_id = api.get('ApiId')
-                    api_arn = self.agent.create_arn('AWS::ApiGatewayV2::Api', self.location_info, api_id)
-                    self.emit_component(api_arn, 'aws.httpapi', make_valid_data(api))
-                    stages = [
-                        stage
-                        for stage in self.client.get_stages(ApiId=api_id)['Items']
-                    ]
-                    routes = [
-                        route
-                        for api_resource_page in self.client.get_paginator(
-                            'get_routes'
-                        ).paginate(ApiId=api_id)
-                        for route in api_resource_page['Items']
-                    ]
-                    integrations = {
-                        integration.get('IntegrationId'): integration
-                        for api_resource_page in self.client.get_paginator(
-                            'get_integrations'
-                        ).paginate(ApiId=api_id)
-                        for integration in api_resource_page['Items']
-                    }
-                    for stage in stages:
-                        deployment_id = stage.get('DeploymentId')
-                        if deployment_id:
-                            stage_name = stage.get('StageName')
-                            stage["Name"] = stage_name
-                            stage_arn = self.agent.create_arn(
-                                'AWS::ApiGatewayV2::Api',
-                                self.location_info,
-                                api_id + '/' + stage_name
-                            )
-                            self.emit_component(stage_arn, 'aws.httpapi.stage', make_valid_data(stage))
-                            self.agent.relation(api_arn, stage_arn, 'has resource', {})
-                            for route in routes:
-                                target = (route.get('Target') or '').split('/')
-                                if len(target) > 1 and integrations[target[-1]]:
-                                    pass
-                                    # print(
-                                    #    stage.get('StageName'),
-                                    #   route.get('RouteKey'),
-                                    #   target[-1],
-                                    #   integrations[target[-1]])
-                    # Integration can be Lambda, HTTP URI, Private Resource in VPC,
-                    #   EventBridge, SQS, AppConfig, Kinesis, StepFunction
-        except Exception as e:
-            print(e)
-            raise e
+        for api in self.collect_apis():
+            self.process_api(api)
+
+    def process_api(self, api_data):
+        api = Api(api_data.summary, strict=False)
+        api.validate()
+        output = make_valid_data(api_data.summary)
+        api_arn = self.agent.create_arn(
+            'AWS::ApiGatewayV2::Api',
+            self.location_info,
+            api.ApiId
+        )
+        self.emit_component(api_arn, 'aws.httpapi', output)
+        for stage in api_data.stages or []:
+            if self.process_stage(api_arn, stage):
+                for route in api_data.routes:
+                    self.process_route(route, api_data.integrations)
+
+    def process_stage(self, api_arn, stage_data):
+        output = make_valid_data(stage_data)
+        stage = Stage(stage_data, strict=False)
+        stage.validate()
+        if stage.DeploymentId:
+            output["Name"] = stage.StageName
+            stage_arn = api_arn + '/' + stage.StageName
+            self.emit_component(stage_arn, 'aws.httpapi.stage', output)
+            self.agent.relation(api_arn, stage_arn, 'has resource', {})
+            return True
+        return False
+
+    def process_route(self, route_data, integrations):
+        route = Route(route_data, strict=False)
+        route.validate()
+        target = route.Target.split('/')
+        if len(target) > 1 and integrations[target[-1]]:
+            pass
+            # print(
+            #    stage.get('StageName'),
+            #   route.get('RouteKey'),
+            #   target[-1],
+            #   integrations[target[-1]])
+            # Integration can be Lambda, HTTP URI, Private Resource in VPC,
+            #   EventBridge, SQS, AppConfig, Kinesis, StepFunction
+
