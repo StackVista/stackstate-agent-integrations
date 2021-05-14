524b96e4f46357d358d5b6bc589e3face79fb9af
Jacco Kulman <jaccokulman@binx.io>
2021-05-14 09:00:41 +0200

    test apigatewayv2

---
+++ aws_topology/tests/test_apigatewayv2.py
@@ -0,0 +1,111 @@
+import unittest
+import os
+import json
+from mock import patch
+from stackstate_checks.base.stubs import topology as top, aggregator
+from stackstate_checks.aws_topology import AwsTopologyCheck, InitConfig
+from stackstate_checks.base import AgentCheck
+import botocore
+import hashlib
+from stackstate_checks.aws_topology.resources.cloudformation import type_arn
+
+
+def get_params_hash(region, data):
+    return hashlib.md5((region + json.dumps(data, sort_keys=True, default=str)).encode('utf-8')).hexdigest()[0:7]
+
+
+def relative_path(path):
+    script_dir = os.path.dirname(__file__)
+    return os.path.join(script_dir, path)
+
+
+def resource(path):
+    with open(relative_path(path)) as f:
+        x = json.load(f)
+    return x
+
+
+def mock_boto_calls(self, *args, **kwargs):
+    if args[0] == "AssumeRole":
+        return {
+            "Credentials": {
+                "AccessKeyId": "KEY_ID",
+                "SecretAccessKey": "ACCESS_KEY",
+                "SessionToken": "TOKEN"
+            }
+        }
+    if args[0] == "LookupEvents":
+        return {}
+    operation_name = botocore.xform_name(args[0])
+    file_name = "json/apigatewayv2/{}_{}.json".format(operation_name, get_params_hash(self.meta.region_name, args))
+    try:
+        return resource(file_name)
+    except Exception:
+        error = "API response file not found for operation: {}\n".format(operation_name)
+        error += "Parameters:\n{}\n".format(json.dumps(args[1], indent=2, default=str))
+        error += "File missing: {}".format(file_name)
+        raise Exception(error)
+
+
+class TestIAM(unittest.TestCase):
+
+    CHECK_NAME = "aws_topology"
+    SERVICE_CHECK_NAME = "aws_topology"
+
+    def setUp(self):
+        """
+        Initialize and patch the check, i.e.
+        """
+        self.patcher = patch("botocore.client.BaseClient._make_api_call", autospec=True)
+        self.mock_object = self.patcher.start()
+        top.reset()
+        aggregator.reset()
+        init_config = InitConfig({
+            "aws_access_key_id": "some_key",
+            "aws_secret_access_key": "some_secret",
+            "external_id": "disable_external_id_this_is_unsafe"
+        })
+        instance = {
+            "role_arn": "arn:aws:iam::548105126730:role/RoleName",
+            "regions": ["eu-west-1"],
+        }
+        instance.update({"apis_to_run": ["apigatewayv2"]})
+
+        self.check = AwsTopologyCheck(self.CHECK_NAME, InitConfig(init_config), [instance])
+        self.mock_object.side_effect = mock_boto_calls
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def assert_executed_ok(self):
+        service_checks = aggregator.service_checks(self.check.SERVICE_CHECK_EXECUTE_NAME)
+        self.assertGreater(len(service_checks), 0)
+        self.assertEqual(service_checks[0].status, AgentCheck.OK, service_checks[0].message)
+
+    def assert_has_component(self, components, id, type):
+        for component in components:
+            if component["id"] == id and component["type"] == type:
+                return component
+        self.assertTrue(False, "Component expected id={} type={}".format(id, type))
+
+    def assert_has_relation(self, relations, source_id, target_id):
+        for relation in relations:
+            if relation["source_id"] == source_id and relation["target_id"] == target_id:
+                return relation
+        self.assertTrue(False, "Relation expected source_id={} target_id={}".format(source_id, target_id))
+
+    def test_process_apigatewayv2(self):
+        self.check.run()
+        topology = [top.get_snapshot(self.check.check_id)]
+        self.assertEqual(len(topology), 1)
+        self.assert_executed_ok()
+
+        print('DONE')
+        components = topology[0]["components"]
+        relations = topology[0]["relations"]
+
+        for component in components:
+            print(component)
+        for relation in relations:
+            print(relation)
+
