1318c7c2cf355b7dbbc4c021a55c25962deddbd0
Elvin Luff <elvin@eelviny.me>
2021-06-03 13:56:49 +0200

    Fix review & IAM permissions

--- aws_topology/stackstate_checks/aws_topology/resources/sns.py
+++ aws_topology/stackstate_checks/aws_topology/resources/sns.py
@@ -16,17 +16,17 @@ def create_arn(region=None, account_id=None, resource_id=None, **kwargs):
     return arn(resource="sns", region=region, account_id=account_id, resource_id=resource_id)
 
 
-TopicData = namedtuple("TopicData", ["topic", "tags"])
+TopicData = namedtuple("TopicData", ["topic", "tags", "subscriptions"])
 
 
 class Topic(Model):
-    TopicArn = StringType()
+    TopicArn = StringType(required=True)
 
 
 class Subscription(Model):
     Endpoint = StringType()
     Protocol = StringType()
-    TopicArn = StringType()
+    TopicArn = StringType(required=True)
 
 
 class SnsCollector(RegisteredResourceCollector):
@@ -43,18 +43,25 @@ class SnsCollector(RegisteredResourceCollector):
     def collect_topic_description(self, topic_arn):
         return self.client.get_topic_attributes(TopicArn=topic_arn).get("Attributes", {})
 
+    def collect_subscriptions(self, topic_arn):
+        for subscription in client_array_operation(
+            self.client, "list_subscriptions_by_topic", "Subscriptions", TopicArn=topic_arn
+        ):
+            yield subscription
+
+    @set_required_access_v2("sns:ListSubscriptionsByTopic")
+    def process_subscriptions(self, topic_arn):
+        return [subscription for subscription in self.collect_subscriptions(topic_arn)]
+
     def collect_topic(self, topic_data):
         topic_arn = topic_data.get("TopicArn")
         # If topic collection fails, just use the object returned by topic_data as this has the ARN
         data = self.collect_topic_description(topic_arn) or topic_data
         # If ARN collection fails, skip tag collection
-        if topic_arn:
-            tags = self.collect_tags(topic_arn)
-        else:
-            tags = []
-        return TopicData(topic=data, tags=tags)
+        tags = self.collect_tags(topic_arn) or []
+        subscriptions = self.process_subscriptions(topic_arn) or []
+        return TopicData(topic=data, tags=tags, subscriptions=subscriptions)
 
-    @set_required_access_v2("sns:ListTopics")
     def collect_topics(self):
         for topic in [
             self.collect_topic(topic_data)
@@ -62,19 +69,14 @@ class SnsCollector(RegisteredResourceCollector):
         ]:
             yield topic
 
-    def collect_subscriptions(self, topic_arn):
-        for subscription in client_array_operation(
-            self.client, "list_subscriptions_by_topic", "Subscriptions", TopicArn=topic_arn
-        ):
-            yield subscription
-
     def process_all(self, filter=None):
-        if not filter or "aws.sns" in filter:
-            for topic_data in self.collect_topics():
-                try:
-                    self.process_topic(topic_data)
-                except Exception:
-                    pass
+        if not filter or "topics" in filter:
+            self.process_topics()
+
+    @set_required_access_v2("sns:ListTopics")
+    def process_topics(self):
+        for topic_data in self.collect_topics():
+            self.process_topic(topic_data)
 
     def process_one_topic(self, topic_arn):
         self.process_topic(self.collect_topic({"TopicArn": topic_arn}))
@@ -91,15 +93,12 @@ class SnsCollector(RegisteredResourceCollector):
         output.update(with_dimensions([{"key": "TopicName", "value": topic_name}]))
         self.emit_component(topic_arn, self.COMPONENT_TYPE, output)
 
-        for subscription_by_topic in self.collect_subscriptions(topic_arn):
-            try:
-                subscription = Subscription(subscription_by_topic, strict=False)
-                subscription.validate()
-                if subscription.Protocol in ["lambda", "sqs"] and subscription.TopicArn == topic_arn:
-                    # TODO subscriptions can be cross region! probably also cross account
-                    self.emit_relation(topic_arn, subscription.Endpoint, "uses service", {})
-            except Exception:
-                pass
+        for subscription_by_topic in data.subscriptions:
+            subscription = Subscription(subscription_by_topic, strict=False)
+            subscription.validate()
+            if subscription.Protocol in ["lambda", "sqs"] and subscription.TopicArn == topic_arn:
+                # TODO subscriptions can be cross region! probably also cross account
+                self.emit_relation(topic_arn, subscription.Endpoint, "uses service", {})
 
     EVENT_SOURCE = "sns.amazonaws.com"
     CLOUDTRAIL_EVENTS = [
