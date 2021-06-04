800be3b0bad895a5080ef0c44fd22edad7fab617
Elvin Luff <elvin@eelviny.me>
2021-06-04 13:02:50 +0200

    Rework elb batching

--- aws_topology/stackstate_checks/aws_topology/resources/elb_classic.py
+++ aws_topology/stackstate_checks/aws_topology/resources/elb_classic.py
@@ -1,5 +1,6 @@
 import time
 from .utils import (
+    client_array_operation,
     make_valid_data,
     create_arn as arn,
     set_required_access_v2,
@@ -41,45 +42,54 @@ class ELBClassicCollector(RegisteredResourceCollector):
     API_TYPE = "regional"
     COMPONENT_TYPE = "aws.elb_classic"
     CLOUDFORMATION_TYPE = "AWS::ElasticLoadBalancing::LoadBalancer"
+    MAX_TAG_CALLS = 20  # This is the max items that can be requested in one describe_tags call
 
     @set_required_access_v2("elasticloadbalancing:DescribeTags")
     def collect_tag_page(self, elb_names):
-        if len(elb_names) <= 20:
+        if len(elb_names) <= self.MAX_TAG_CALLS:
             return self.client.describe_tags(LoadBalancerNames=elb_names).get("TagDescriptions", [])
         else:
             return []
 
     @set_required_access_v2("elasticloadbalancing:DescribeLoadBalancers")
-    def collect_elb_page(self, **kwargs):
-        # This doesn't use the utils.client_array_operation function as we need control over grabbing one page at a time
-        for elb_page in self.client.get_paginator("describe_load_balancers").paginate(
-            PaginationConfig={
-                "MaxItems": 20,  # Use 20 to match the describe_tags API call, where max items in one call is also 20
-            },
-            **kwargs
-        ):
-            yield elb_page.get("LoadBalancerDescriptions", [])
+    def collect_elbs(self, **kwargs):
+        for elb in client_array_operation(self.client, "describe_load_balancers", "LoadBalancerDescriptions", **kwargs):
+            yield elb
 
     @set_required_access_v2("elasticloadbalancing:DescribeInstanceHealth")
     def collect_instance_health(self, elb_name):
         return self.client.describe_instance_health(LoadBalancerName=elb_name).get("InstanceStates", [])
 
+    def process_elb_page(self, elbs):
+        tag_page = self.collect_tag_page([elb.get("LoadBalancerName", "") for elb in elbs])
+        for data in elbs:
+            instance_health = self.collect_instance_health(data.get("LoadBalancerName"))
+            tags = []
+            # Match the result from the fetched tags with the specific ELB
+            for tag_result in tag_page:
+                if tag_result.get("LoadBalancerName") == data.get("LoadBalancerName"):
+                    tags = tag_result.get("Tags", [])
+            self.process_elb(LoadBalancerData(elb=data, tags=tags, instance_health=instance_health))
+
     def process_elbs(self, elb_names=[]):
         if elb_names:  # Only pass in LoadBalancerNames if a specific name is needed, otherwise ask for all
-            paginator = self.collect_elb_page(LoadBalancerNames=elb_names)
+            paginator = self.collect_elbs(LoadBalancerNames=elb_names)
         else:
-            paginator = self.collect_elb_page()
-        for elb_page in paginator or []:
-            # The paginator fetches max 20 items, which is the max items that can be fetched with describe_tags
-            tag_page = self.collect_tag_page([elb.get("LoadBalancerName", "") for elb in elb_page])
-            for data in elb_page:
-                instance_health = self.collect_instance_health(data.get("LoadBalancerName"))
-                tags = []
-                # Match the result from the fetched tags with the specific ELB
-                for tag_result in tag_page:
-                    if tag_result.get("LoadBalancerName") == data.get("LoadBalancerName"):
-                        tags = tag_result.get("Tags", [])
-                self.process_elb(LoadBalancerData(elb=data, tags=tags, instance_health=instance_health))
+            paginator = self.collect_elbs()
+        elbs = []
+        while True:
+            try:
+                elb = next(paginator)
+                # Batch up elbs into groups, then process them in pages of 20
+                if len(elbs) >= self.MAX_TAG_CALLS:
+                    self.process_elb_page(elbs)
+                    elbs = []
+                else:
+                    elbs.append(elb)
+            except StopIteration:
+                # If we run out of elbs to process, process the last ones then break
+                self.process_elb_page(elbs)
+                break
 
     def process_all(self, filter=None):
         if not filter or "loadbalancers" in filter:
@@ -93,17 +103,14 @@ class ELBClassicCollector(RegisteredResourceCollector):
         elb = LoadBalancer(data.elb, strict=False)
         elb.validate()
         output = make_valid_data(data.elb)
-        elb_name = elb.LoadBalancerName
         elb_arn = self.agent.create_arn(
-            "AWS::ElasticLoadBalancing::LoadBalancer", self.location_info, resource_id=elb_name
+            "AWS::ElasticLoadBalancing::LoadBalancer", self.location_info, resource_id=elb.LoadBalancerName
         )
-        output["Name"] = elb_name
+        output["Name"] = elb.LoadBalancerName
         output["Tags"] = data.tags
         output["URN"] = [elb_arn]
         self.emit_component(elb_arn, self.COMPONENT_TYPE, output)
-
-        vpc_id = elb.VPCId
-        self.emit_relation(elb_arn, vpc_id, "uses service", {})
+        self.emit_relation(elb_arn, elb.VPCId, "uses service", {})
 
         for instance in output.get("Instances", []):
             instance_external_id = instance.get("InstanceId")  # ec2 instance
