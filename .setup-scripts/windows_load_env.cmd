6892cfa64d659720fd28255959cb5de91a3d542e
Vyacheslav Voronenko <git@voronenko.info>
2021-06-10 11:01:23 +0200

    try to retry check_dev installation in scope of single run

--- .setup-scripts/windows_load_env.cmd
+++ .setup-scripts/windows_load_env.cmd
@@ -1,2 +1,7 @@
 call pip install .\stackstate_checks_dev[cli]
-
+IF %ERRORLEVEL% NEQ 0 (
+  call pip install .\stackstate_checks_dev[cli]
+)
+IF %ERRORLEVEL% NEQ 0 (
+  call pip install .\stackstate_checks_dev[cli]
+)
