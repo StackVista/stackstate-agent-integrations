1e2f9fb1440c3135a66aa0722b1636383cd19b34
Elvin Luff <elvin@eelviny.me>
2021-06-11 16:02:26 +0200

    Refactor and simplify cloudformation

--- aws_topology/tests/test_cloudformation.py
+++ aws_topology/tests/test_cloudformation.py
@@ -52,6 +52,7 @@ class TestCloudFormation(BaseApiTest):
             checks={"LastUpdatedTime": "2019-06-27T20:20:45.336Z", "StackName": "stackstate-topo-cwevents"},
         )
 
-        top.assert_relation(relations, stack1, stack2, "child of")
+        print(relations)
+        top.assert_relation(relations, stack2, stack1, "has resource")
 
         top.assert_all_checked(components, relations)
