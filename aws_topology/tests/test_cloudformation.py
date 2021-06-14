a0030dee05d53340450fa75f10d4deef1df8269e
Elvin Luff <elvin@eelviny.me>
2021-06-14 12:41:50 +0200

    Add more relation tests

--- aws_topology/tests/test_cloudformation.py
+++ aws_topology/tests/test_cloudformation.py
@@ -52,7 +52,90 @@ class TestCloudFormation(BaseApiTest):
             checks={"LastUpdatedTime": "2019-06-27T20:20:45.336Z", "StackName": "stackstate-topo-cwevents"},
         )
 
-        print(relations)
         top.assert_relation(relations, stack2, stack1, "has resource")
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:lambda:eu-west-1:731070500579:function:com-stackstate-prod-sam-seed-PutHello-1LUD3ESBOR6EY",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:kinesis:eu-west-1:731070500579:stream/stream_1",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:s3:::stackstate.com",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:execute-api:eu-west-1:731070500579:api_1",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:elasticloadbalancing:eu-west-1:731070500579:targetgroup/myfirsttargetgroup/28ddec997ec55d21",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:elasticloadbalancing:eu-west-1:731070500579:loadbalancer/app/myfirstloadbalancer/90dd512583d2d7e9",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "awseb-e-gwhbyckyjq-stack-AWSEBAutoScalingGroup-35ZMDUKHPCUM",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:elasticloadbalancing:eu-west-1:731070500579:loadbalancer/classic-loadbalancer-1",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:rds:eu-west-1:731070500579:db:productiondatabase",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:sns:eu-west-1:731070500579:my-topic-3",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:dynamodb:eu-west-1:731070500579:table/table_3",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "i-0aac5bab082561475",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:sqs:eu-west-1:731070500579:STS_stackpack_test",
+            "has resource",
+        )
+        top.assert_relation(
+            relations,
+            stack1,
+            "arn:aws:ecs:eu-west-1:731070500579:cluster/StackState-ECS-Cluster",
+            "has resource",
+        )
 
         top.assert_all_checked(components, relations)
