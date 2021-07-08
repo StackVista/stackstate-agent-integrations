39dc82e0e791fa6c92940e6436e35f6b6740a4ad
Jacco Kulman <jaccokulman@binx.io>
2021-07-08 14:28:28 +0200

    test added for cluster new URNs

--- aws_topology/tests/test_rds.py
+++ aws_topology/tests/test_rds.py
@@ -28,6 +28,10 @@ class TestRds(BaseApiTest):
                 "Name": "arn:aws:rds:eu-west-1:731070500579:cluster:productiondatabasecluster",
                 "CW.Dimensions": [{"Key": "DBClusterIdentifier", "Value": "productiondatabasecluster"}],
                 "Tags.testing": "test",
+                "URN": [
+                    'urn:endpoint:/productiondatabasecluster.cluster-cdnm1uvvpdkc.eu-west-1.rds.amazonaws.com',
+                    'urn:endpoint:/productiondatabasecluster.cluster-ro-cdnm1uvvpdkc.eu-west-1.rds.amazonaws.com'
+                ],
             },
         )
         # instance 1
