ffd76384ed72cb236519dd27b977532ee10516c0
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-08-02 15:04:27 +0200

    Extracted CMDB_CI tables mock calls

--- servicenow/tests/test_snow_change_requests.py
+++ servicenow/tests/test_snow_change_requests.py
@@ -10,10 +10,7 @@ SERVICE_CHECK_NAME = "servicenow.cmdb.topology_information"
 
 def test_planned_crs(servicenow_check, requests_mock, test_cr_instance):
     url = test_cr_instance.get('url')
-    api_cmdb_ci_url = url + API_SNOW_TABLE_CMDB_CI
-    requests_mock.register_uri('GET', api_cmdb_ci_url, status_code=200, text='{"result": []}')
-    api_cmdb_ci_rel_url = url + API_SNOW_TABLE_CMDB_REL_CI
-    requests_mock.register_uri('GET', api_cmdb_ci_rel_url, status_code=200, text='{"result": []}')
+    request_mock_cmdb_ci_tables_setup(requests_mock, url)
     api_cr_url = url + API_SNOW_TABLE_CHANGE_REQUEST
     requests_mock.register_uri('GET', api_cr_url, [
         {'status_code': 200, 'text': read_file('CHG0040007.json', 'samples')},
@@ -21,3 +18,10 @@ def test_planned_crs(servicenow_check, requests_mock, test_cr_instance):
     ])
     servicenow_check.run()
     aggregator.assert_service_check(SERVICE_CHECK_NAME, count=1, status=AgentCheck.OK)
+
+
+def request_mock_cmdb_ci_tables_setup(requests_mock, url):
+    api_cmdb_ci_url = url + API_SNOW_TABLE_CMDB_CI
+    requests_mock.register_uri('GET', api_cmdb_ci_url, status_code=200, text='{"result": []}')
+    api_cmdb_ci_rel_url = url + API_SNOW_TABLE_CMDB_REL_CI
+    requests_mock.register_uri('GET', api_cmdb_ci_rel_url, status_code=200, text='{"result": []}')
