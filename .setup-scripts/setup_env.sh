159ffba62a9f2d139de04087971df72129e43a81
zandrewitte <zandre.witte@gmail.com>
2021-08-31 13:46:49 +0200

    echo venv status

--- .setup-scripts/setup_env.sh
+++ .setup-scripts/setup_env.sh
@@ -9,13 +9,18 @@ export INTEGRATIONS_DIR_TMP=${CI_PROJECT_DIR:-"."}
 VENV_PATH=$INTEGRATIONS_DIR_TMP/venv
 
 if [ ! -d $VENV_PATH ]; then
-  echo '$VENV_PATH doesnt exist, create the venv and loading deps'
+  echo "$VENV_PATH doesn't exist, create the venv and loading deps"
   virtualenv --python=python3 $INTEGRATIONS_DIR_TMP/venv
   source $INTEGRATIONS_DIR_TMP/venv/bin/activate
   source $INTEGRATIONS_DIR_TMP/.setup-scripts/load_deps.sh
 else
-  echo '$VENV_PATH already exists, only activating the venv'
+  echo "$VENV_PATH already exists, only activating the venv"
+  pip -v
+  pip -V
   source $INTEGRATIONS_DIR_TMP/venv/bin/activate
+  pip -v
+  pip -V
+  checksdev -h
 fi
 
 unset INTEGRATIONS_DIR_TMP
