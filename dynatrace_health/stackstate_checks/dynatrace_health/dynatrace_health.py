94ff89a7f6a9f9d1ad4e7e67fa3be740df095473
Hrvoje Ruhek <39823032+hruhek@users.noreply.github.com>
2021-10-14 09:56:16 +0200

    STAC-14104 - Split Dynatrace check into topology and events checks (#232)
    * Extracted rest api call method to DynatraceClient call
    
    * Split client code to base and added topology check
    
    * Events as separate check
    
    * removed old dynatrace check
    
    * added new dynatrace checks to pipeline

---
+++ dynatrace_health/stackstate_checks/dynatrace_health/dynatrace_health.py
@@ -0,0 +1,265 @@
+# (C) StackState 2021
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import time
+from datetime import datetime, timedelta
+
+from schematics import Model
+from schematics.types import IntType, StringType, ListType, DictType, URLType, BooleanType, ModelType
+
+from stackstate_checks.base import StackPackInstance, HealthStream, HealthStreamUrn, Health, Identifiers
+from stackstate_checks.checks import AgentCheck
+from stackstate_checks.dynatrace.dynatrance_client import DynatraceClient
+
+VERIFY_HTTPS = True
+TIMEOUT = 10
+EVENTS_BOOSTRAP_DAYS = 5
+EVENTS_PROCESS_LIMIT = 10000
+RELATIVE_TIME = 'hour'
+
+DYNATRACE_UI_URLS = {
+    "SERVICE": "%s/#newservices/serviceOverview;id=%s",
+    "PROCESS_GROUP": "%s/#processgroupdetails;id=%s",
+    "PROCESS_GROUP_INSTANCE": "%s/#processdetails;id=%s",
+    "PROCESS": "%s/#processdetails;id=%s",
+    "HOST": "%s/#newhosts/hostdetails;id=%s",
+    "APPLICATION": "%s/#uemapplications/uemappmetrics;uemapplicationId=%s",
+    "CUSTOM_DEVICE": "%s/#customdevicegroupdetails/entity;id=%s"
+}
+
+
+class DynatraceEvent(Model):
+    eventId = IntType()
+    startTime = IntType()
+    endTime = IntType()
+    entityId = StringType()
+    entityName = StringType()
+    severityLevel = StringType()
+    impactLevel = StringType()
+    eventType = StringType()
+    eventStatus = StringType()
+    tags = ListType(DictType(StringType))
+    id = StringType()
+    source = StringType()
+
+
+class State(Model):
+    last_processed_event_timestamp = IntType(required=True)
+
+
+class InstanceInfo(Model):
+    url = URLType(required=True)
+    token = StringType(required=True)
+    instance_tags = ListType(StringType, default=[])
+    events_boostrap_days = IntType(default=EVENTS_BOOSTRAP_DAYS)
+    events_process_limit = IntType(default=EVENTS_PROCESS_LIMIT)
+    verify = BooleanType(default=VERIFY_HTTPS)
+    cert = StringType()
+    keyfile = StringType()
+    timeout = IntType(default=TIMEOUT)
+    relative_time = StringType(default=RELATIVE_TIME)
+    state = ModelType(State)
+
+
+class DynatraceHealthCheck(AgentCheck):
+    INSTANCE_TYPE = "dynatrace"
+    SERVICE_CHECK_NAME = "dynatrace-health"
+    INSTANCE_SCHEMA = InstanceInfo
+
+    def get_instance_key(self, instance_info):
+        return StackPackInstance(self.INSTANCE_TYPE, str(instance_info.url))
+
+    def get_health_stream(self, instance):
+        return HealthStream(HealthStreamUrn(self.INSTANCE_TYPE, 'dynatrace-events'))
+
+    def check(self, instance_info):
+        try:
+            if not instance_info.state:
+                # Create state on the first run
+                empty_state_timestamp = self.generate_bootstrap_timestamp(instance_info.events_boostrap_days)
+                self.log.debug('Creating new empty state with timestamp: %s', empty_state_timestamp)
+                instance_info.state = State({'last_processed_event_timestamp': empty_state_timestamp})
+            dynatrace_client = DynatraceClient(instance_info.token,
+                                               instance_info.verify,
+                                               instance_info.cert,
+                                               instance_info.keyfile,
+                                               instance_info.timeout)
+            self._process_events(dynatrace_client, instance_info)
+            msg = "Dynatrace health check processed successfully"
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK, tags=instance_info.instance_tags, message=msg)
+        except EventLimitReachedException as e:
+            self.log.exception(str(e))
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.WARNING, tags=instance_info.instance_tags,
+                               message=str(e))
+        except Exception as e:
+            self.log.exception(str(e))
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL, tags=instance_info.instance_tags,
+                               message=str(e))
+
+    def _process_events(self, dynatrace_client, instance_info):
+        """
+        Wrapper to collect events, filters those events and persist the state
+        """
+        severity_levels_that_maps_to_deviating_health_state = ["PERFORMANCE", "RESOURCE_CONTENTION",
+                                                               "MONITORING_UNAVAILABLE", "ERROR"]
+        severity_levels_that_maps_to_critical_health_state = ["AVAILABILITY", "CUSTOM_ALERT"]
+        events, events_limit_reached = self._collect_events(dynatrace_client, instance_info)
+        open_events = [e for e in events if e.get('eventStatus') == 'OPEN']
+        closed_events = len(events) - len(open_events)
+        self.log.info("Collected %d events, %d are open and %d are closed.", len(events), len(open_events),
+                      closed_events)
+        self.health.start_snapshot()
+        for event in open_events:
+            if event.severityLevel == 'INFO':
+                # Events with a info severity are send as topology events
+                link_to_entity = self.link_to_dynatrace(str(event.entityId), instance_info.url)
+                self._create_topology_event(event, link_to_entity)
+            else:
+                # Create health state for other events
+                if event.severityLevel in severity_levels_that_maps_to_deviating_health_state:
+                    health_value = Health.DEVIATING
+                elif event.severityLevel in severity_levels_that_maps_to_critical_health_state:
+                    health_value = Health.CRITICAL
+                else:
+                    health_value = Health.CLEAR
+                identifier = Identifiers.create_custom_identifier("dynatrace", event.entityId)
+                self.health.check_state(
+                    check_state_id=event.entityId,
+                    name='Dynatrace event',
+                    health_value=health_value,
+                    topology_element_identifier=identifier,
+                    message='Event: {} Severity: {} Impact: {} Open Since: {} Source: {}'.format(
+                        event.eventType, event.severityLevel, event.impactLevel, event.startTime, event.source
+                    )
+                )
+        self.health.stop_snapshot()
+        if events_limit_reached:
+            raise EventLimitReachedException(events_limit_reached)
+
+    def _create_topology_event(self, dynatrace_event, link_to_entity):
+        """
+        Create an standard or custom event based on the Dynatrace Severity level
+        """
+        event = {
+            "timestamp": int(time.time()),
+            "source_type_name": "Dynatrace Events",
+            "msg_title": "%s on %s" % (dynatrace_event.eventType, dynatrace_event.entityName),
+            "msg_text": "%s on %s" % (dynatrace_event.eventType, dynatrace_event.entityName),
+            "tags": [
+                "entityId:%s" % dynatrace_event.entityId,
+                "severityLevel:%s" % dynatrace_event.severityLevel,
+                "eventType:%s" % dynatrace_event.eventType,
+                "impactLevel:%s" % dynatrace_event.impactLevel,
+                "eventStatus:%s" % dynatrace_event.eventStatus,
+                "startTime:%s" % dynatrace_event.startTime,
+                "endTime:%s" % dynatrace_event.endTime,
+                "source:%s" % dynatrace_event.source,
+                "openSince:%s" % datetime.fromtimestamp(dynatrace_event.startTime / 1000).strftime(
+                    "%b %-d, %Y, %H:%M:%S"),
+            ],
+            "context": {
+                "source_identifier": "source_identifier_value",
+                "element_identifiers": ["urn:%s" % dynatrace_event.entityId],
+                "source": "dynatrace",
+                "category": "info_event",
+                "data": dynatrace_event.to_primitive(),
+                "source_links": [
+                    {
+                        "title": "my_event_external_link",
+                        "url": link_to_entity
+                    }
+                ]
+            }
+        }
+        self.event(event)
+
+    def _collect_events(self, dynatrace_client, instance_info):
+        """
+        Checks for EventLimitReachedException and process each event API response for next cursor
+        until is None or it reach events_process_limit
+        """
+        events_response = self._get_events(dynatrace_client, instance_info.url,
+                                           from_time=instance_info.state.last_processed_event_timestamp)
+        new_events = []
+        events_processed = 0
+        event_limit_reached = None
+        try:
+            while events_response:
+                events = events_response.get('events', [])
+                for event in events:
+                    dynatrace_event = DynatraceEvent(event, strict=False)
+                    dynatrace_event.validate()
+                    new_events.append(dynatrace_event)
+                    events_processed += 1
+                    self._check_event_limit_exceeded_condition(instance_info.events_process_limit, events_processed)
+                if events_response.get("nextCursor"):
+                    events_response = self._get_events(dynatrace_client, instance_info.url,
+                                                       cursor=events_response.get("nextCursor"))
+                else:
+                    instance_info.state.last_processed_event_timestamp = events_response.get("to")
+                    events_response = None
+        except EventLimitReachedException as e:
+            instance_info.state.last_processed_event_timestamp = events_response.get("to")
+            event_limit_reached = str(e)
+        return new_events, event_limit_reached
+
+    def _get_events(self, dynatrace_client, url, from_time=None, cursor=None):
+        """
+        Get events from Dynatrace Event API endpoint
+        :param dynatrace_client: dynatrace rest client
+        :param url: dynatrace instance url
+        :param from_time: timestamp from which to collect events
+        :param cursor: batch cursor
+        :return: Event API endpoint response
+        """
+        params = {}
+        if from_time:
+            params['from'] = from_time
+        if cursor:
+            params['cursor'] = cursor
+        endpoint = dynatrace_client.get_endpoint(url, "/api/v1/events")
+        events = dynatrace_client.get_dynatrace_json_response(endpoint, params)
+        self.log.debug('Got %s events from %s', len(events.get('events', [])), endpoint)
+        return events
+
+    @staticmethod
+    def _check_event_limit_exceeded_condition(events_process_limit, total_event_count):
+        """
+        Raises EventLimitReachedException if number of events between subsequent check runs
+        exceed the `events_process_limit`
+        """
+        if total_event_count > events_process_limit:
+            raise EventLimitReachedException("Maximum event limit to process is %s but received total %s events"
+                                             % (events_process_limit, total_event_count))
+
+    @staticmethod
+    def link_to_dynatrace(entity_id, instance_url):
+        """
+        Compose url to dynatrace entity page, if not able return link to Dynatrace instance.
+        :param entity_id: Dynatrace entity identifier
+        :param instance_url: Dynatrace instance url
+        :return: url to Dynatrace entity page.
+        """
+        entity_type = entity_id.split("-")[0]
+        try:
+            url = DYNATRACE_UI_URLS[entity_type] % (instance_url, entity_id)
+            return url
+        except KeyError:
+            return instance_url
+
+    @staticmethod
+    def generate_bootstrap_timestamp(days):
+        """
+        Creates timestamp n days in the past from the current moment. It is used in tests too.
+        :param days: how many days in the past
+        :return:
+        """
+        bootstrap_date = datetime.fromtimestamp(int(time.time())) - timedelta(days=days)
+        return int(bootstrap_date.strftime('%s')) * 1000
+
+
+class EventLimitReachedException(Exception):
+    """
+    Exception raised when maximum number of event reached
+    """
+    pass
---
+++ dynatrace_health/tests/test_dynatrace_health.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+
+# (C) StackState 2021
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import requests
+from freezegun import freeze_time
+
+from stackstate_checks.base import AgentCheck, StateDescriptor
+from stackstate_checks.base.utils.common import read_file, load_json_from_file
+from stackstate_checks.dynatrace_health import State
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_no_events_means_empty_health_snapshot(dynatrace_check, test_instance, requests_mock, health, aggregator):
+    """
+    Dynatrace health check should not produce any health states when there are no events
+    """
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    requests_mock.get('{}/api/v1/events?from={}'.format(test_instance['url'], timestamp), status_code=200,
+                      text=read_file('no_events_response.json', 'samples'))
+    dynatrace_check.run()
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.OK)
+    health.assert_snapshot(dynatrace_check.check_id, dynatrace_check.health.stream,
+                           check_states=[],
+                           start_snapshot={'expiry_interval_s': 60, 'repeat_interval_s': 15},
+                           stop_snapshot={})
+    assert len(aggregator.events) == 0
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_events_process_limit(dynatrace_check, test_instance, aggregator, requests_mock, health):
+    """
+    Check should respect `events_process_limit` config setting and just produce those number of events
+    """
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    requests_mock.get('{}/api/v1/events?from={}'.format(test_instance['url'], timestamp), status_code=200,
+                      text=read_file('21_events_response.json', 'samples'))
+    dynatrace_check.run()
+    # service check returns warning about events_process_limit
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.WARNING,
+                                    message='Maximum event limit to process is 10 but received total 11 events')
+    # 2 open events were in first 10 events gathered -> 2 health states
+    health.assert_snapshot(dynatrace_check.check_id, dynatrace_check.health.stream,
+                           check_states=[{'checkStateId': 'SERVICE-FAA29C9BB1C02F9B',
+                                          'health': 'DEVIATING',
+                                          'message': 'Event: FAILURE_RATE_INCREASED Severity: ERROR '
+                                                     'Impact: SERVICE Open Since: 1600874700000 '
+                                                     'Source: builtin',
+                                          'name': 'Dynatrace event',
+                                          'topologyElementIdentifier': 'urn:dynatrace:/SERVICE-FAA29C9BB1C02F9B'},
+                                         {'checkStateId': 'SERVICE-9B16B9C5B03836C5',
+                                          'health': 'DEVIATING',
+                                          'message': 'Event: FAILURE_RATE_INCREASED Severity: ERROR '
+                                                     'Impact: SERVICE Open Since: 1600874820000 '
+                                                     'Source: builtin',
+                                          'name': 'Dynatrace event',
+                                          'topologyElementIdentifier': 'urn:dynatrace:/SERVICE-9B16B9C5B03836C5'}],
+                           start_snapshot={'expiry_interval_s': 60, 'repeat_interval_s': 15},
+                           stop_snapshot={})
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_events_process_limit_with_batches(dynatrace_check, test_instance, requests_mock, aggregator, health):
+    """
+    Respecting `events_process_limit` config setting should happen with batch event retrieval too
+    """
+    url = test_instance['url']
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    # there are 3 batch calls with total 12 events, open events are 3rd and 12th in collection
+    url1 = '{}/api/v1/events?from={}'.format(url, timestamp)
+    url2 = '{}/api/v1/events?cursor={}'.format(url, '123')
+    url3 = '{}/api/v1/events?cursor={}'.format(url, '345')
+    requests_mock.get(url1, status_code=200, text=read_file('events_set1_response.json', 'samples'))
+    requests_mock.get(url2, status_code=200, text=read_file('events_set2_response.json', 'samples'))
+    requests_mock.get(url3, status_code=200, text=read_file('events_set3_response.json', 'samples'))
+    dynatrace_check.run()
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.WARNING,
+                                    message='Maximum event limit to process is 10 but received total 11 events')
+    # 1 open event were in first 10 events gathered -> 1 health states
+    health.assert_snapshot(dynatrace_check.check_id, dynatrace_check.health.stream,
+                           check_states=[{'checkStateId': 'SERVICE-FAA29C9BB1C02F9B',
+                                          'health': 'DEVIATING',
+                                          'message': 'Event: FAILURE_RATE_INCREASED Severity: ERROR '
+                                                     'Impact: SERVICE Open Since: 1600874700000 '
+                                                     'Source: builtin',
+                                          'name': 'Dynatrace event',
+                                          'topologyElementIdentifier': 'urn:dynatrace:/SERVICE-FAA29C9BB1C02F9B'}
+                                         ],
+                           start_snapshot={'expiry_interval_s': 60, 'repeat_interval_s': 15},
+                           stop_snapshot={})
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_raise_exception_for_response_code_not_200(dynatrace_check, test_instance, requests_mock, aggregator):
+    """
+    Test to raise a check exception when API endpoint when status code is not 200
+    """
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    requests_mock.get('{}/api/v1/events?from={}'.format(test_instance['url'], timestamp),
+                      status_code=500, text='{"error": {"code": 500, "message": "Simulated error!"}}')
+    dynatrace_check.run()
+    error_message = 'Got an unexpected error with status code 500 and message: Simulated error!'
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.CRITICAL,
+                                    message=error_message)
+    assert len(aggregator.events) == 0
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_generated_events(dynatrace_check, test_instance, requests_mock, aggregator, telemetry, health):
+    """
+    Testing Dynatrace check should produce full events
+    """
+    url = test_instance['url']
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    requests_mock.get('{}/api/v1/events?from={}'.format(url, timestamp), status_code=200,
+                      text=read_file('9_events_response.json', 'samples'))
+    dynatrace_check.run()
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.OK)
+    health.assert_snapshot(dynatrace_check.check_id, dynatrace_check.health.stream,
+                           check_states=[{'checkStateId': 'HOST-1E81568BDBFFE7FC',
+                                          'health': 'DEVIATING',
+                                          'message': 'Event: CPU_SATURATED Severity: '
+                                                     'RESOURCE_CONTENTION Impact: INFRASTRUCTURE Open '
+                                                     'Since: 1613466660000 Source: builtin',
+                                          'name': 'Dynatrace event',
+                                          'topologyElementIdentifier': 'urn:dynatrace:/HOST-1E81568BDBFFE7FC'},
+                                         {'checkStateId': 'HOST-AA7D1491C9FE6BC4',
+                                          'health': 'CRITICAL',
+                                          'message': 'Event: CONNECTION_LOST Severity: AVAILABILITY '
+                                                     'Impact: INFRASTRUCTURE Open Since: '
+                                                     '1613430060468 Source: builtin',
+                                          'name': 'Dynatrace event',
+                                          'topologyElementIdentifier': 'urn:dynatrace:/HOST-AA7D1491C9FE6BC4'},
+                                         {'checkStateId': 'HOST-8F3EDA24EEF51137',
+                                          'health': 'CRITICAL',
+                                          'message': 'Event: CONNECTION_LOST Severity: AVAILABILITY '
+                                                     'Impact: INFRASTRUCTURE Open Since: '
+                                                     '1613430060467 Source: builtin',
+                                          'name': 'Dynatrace event',
+                                          'topologyElementIdentifier': 'urn:dynatrace:/HOST-8F3EDA24EEF51137'}],
+                           start_snapshot={'expiry_interval_s': 60, 'repeat_interval_s': 15},
+                           stop_snapshot={})
+    assert len(telemetry._topology_events) == 1
+    expected_topology_events = load_json_from_file('expected_topology_events.json', 'samples')
+    for expected_event in expected_topology_events:
+        assert expected_event == telemetry._topology_events[0]
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_state_data(state, dynatrace_check, test_instance, requests_mock, aggregator):
+    """
+    Check is the right timestamp is writen to the state
+    """
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    state_instance = StateDescriptor("instance.dynatrace_health.https_instance.live.dynatrace.com", "dynatrace.d")
+    state.assert_state(state_instance, None)
+    events_file = 'no_events_response.json'
+    requests_mock.get('{}/api/v1/events?from={}'.format(test_instance['url'], timestamp), status_code=200,
+                      text=read_file(events_file, 'samples'))
+    dynatrace_check.run()
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.OK)
+    mocked_response_data = load_json_from_file(events_file, 'samples')
+    new_state = State({'last_processed_event_timestamp': mocked_response_data.get('to')})
+    state.assert_state(state_instance, new_state)
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_timeout(dynatrace_check, test_instance, requests_mock, aggregator):
+    """
+    Gracefully handle requests timeout exception
+    """
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    requests_mock.get('{}/api/v1/events?from={}'.format(test_instance['url'], timestamp),
+                      exc=requests.exceptions.ConnectTimeout)
+    dynatrace_check.run()
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.CRITICAL,
+                                    message='Timeout exception occurred for endpoint '
+                                            'https://instance.live.dynatrace.com/api/v1/events with message: '
+                                            '20 seconds timeout')
+
+
+def test_link_to_dynatrace(dynatrace_check, test_instance):
+    url = test_instance['url']
+
+    service_url = dynatrace_check.link_to_dynatrace("SERVICE-123", url)
+    assert service_url == "https://instance.live.dynatrace.com/#newservices/serviceOverview;id=SERVICE-123"
+
+    process_group_url = dynatrace_check.link_to_dynatrace("PROCESS_GROUP-456", url)
+    assert process_group_url == "https://instance.live.dynatrace.com/#processgroupdetails;id=PROCESS_GROUP-456"
+
+    process_url = dynatrace_check.link_to_dynatrace("PROCESS-789", url)
+    assert process_url == "https://instance.live.dynatrace.com/#processdetails;id=PROCESS-789"
+
+    process_url = dynatrace_check.link_to_dynatrace("PROCESS_GROUP_INSTANCE-000", url)
+    assert process_url == "https://instance.live.dynatrace.com/#processdetails;id=PROCESS_GROUP_INSTANCE-000"
+
+    host_url = dynatrace_check.link_to_dynatrace("HOST-abc", url)
+    assert host_url == "https://instance.live.dynatrace.com/#newhosts/hostdetails;id=HOST-abc"
+
+    application_url = dynatrace_check.link_to_dynatrace("APPLICATION-def", url)
+    assert application_url == "https://instance.live.dynatrace.com/#uemapplications/uemappmetrics;uemapplicationId=" \
+                              "APPLICATION-def"
+
+    custom_device_url = dynatrace_check.link_to_dynatrace("CUSTOM_DEVICE-abc", url)
+    assert custom_device_url == 'https://instance.live.dynatrace.com/#customdevicegroupdetails/entity;id=' \
+                                'CUSTOM_DEVICE-abc'
+
+
+def test_link_to_dynatrace_unknown_type(dynatrace_check, test_instance):
+    instance_url = test_instance.get('url')
+    process_url = dynatrace_check.link_to_dynatrace('UNKNOWN', instance_url)
+    assert process_url == instance_url
+
+
+@freeze_time('2021-02-16 14:26:24')
+def test_unicode_in_response_text(dynatrace_check, test_instance, requests_mock, aggregator, telemetry):
+    timestamp = dynatrace_check.generate_bootstrap_timestamp(test_instance['events_boostrap_days'])
+    requests_mock.get('{}/api/v1/events?from={}'.format(test_instance['url'], timestamp), status_code=200,
+                      text=read_file('unicode_topology_event_response.json', 'samples'))
+    dynatrace_check.run()
+    aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.OK)
+    assert telemetry._topology_events[0]['msg_text'] == 'PROCESS_RESTART on aws-cni™'
