63fc94dbd568f395c146dee3048e53cba0ecfb51
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-12-07 14:19:48 +0100

    added debug msg for artifactory URL

--- .setup-scripts/setup_vsphere_artifactory_publishing.sh
+++ .setup-scripts/setup_vsphere_artifactory_publishing.sh
@@ -1,4 +1,5 @@
 ## python artifactory dependency
+echo "Artifactory URL: $artifactory_url"
 mkdir ~/.pip/ && touch ~/.pip/pip.conf
 echo "[global]" > ~/.pip/pip.conf
-echo "extra-index-url = https://$artifactory_user:$artifactory_password@$artifactory_url/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
+echo "extra-index-url = https://$artifactory_user:$artifactory_password@$artifactory_url/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
