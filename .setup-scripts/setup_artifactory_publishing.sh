5ab348769efa3f363a57265260ab5b598ca76662
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-12-07 12:29:41 +0100

    fixed artifactorr url in scripts

--- .setup-scripts/setup_artifactory_publishing.sh
+++ .setup-scripts/setup_artifactory_publishing.sh
@@ -1,12 +1,12 @@
 ## python artifactory dependency
 mkdir ~/.pip/ && touch ~/.pip/pip.conf
 echo "[global]" > ~/.pip/pip.conf
-echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$ARTIFACTORY_URL/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
+echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$artifactory_url/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
 
 touch ~/.pypirc
 echo "[distutils]" > ~/.pypirc
 echo "index-servers = local" >> ~/.pypirc
 echo "[local]" >> ~/.pypirc
-echo "repository: https://$ARTIFACTORY_URL" >> ~/.pypirc
+echo "repository: https://$artifactory_url" >> ~/.pypirc
 echo "username: $ARTIFACTORY_USER" >> ~/.pypirc
 echo "password: $ARTIFACTORY_PASSWORD" >> ~/.pypirc
