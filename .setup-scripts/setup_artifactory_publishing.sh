4b8931303ea2bcad0188cc27bdfd6c51a72b095d
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-12-07 11:23:58 +0100

    use global github ARTIFACTORY_URL env var

--- .setup-scripts/setup_artifactory_publishing.sh
+++ .setup-scripts/setup_artifactory_publishing.sh
@@ -1,7 +1,7 @@
 ## python artifactory dependency
 mkdir ~/.pip/ && touch ~/.pip/pip.conf
 echo "[global]" > ~/.pip/pip.conf
-echo "index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$ARTIFACTORY_URL/simple" >> ~/.pip/pip.conf
+echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$ARTIFACTORY_URL/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
 
 touch ~/.pypirc
 echo "[distutils]" > ~/.pypirc
