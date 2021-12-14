52a05e1f305f0098379cbbfd9dbea6fe7d54a29c
Hrvoje Ruhek <39823032+hruhek@users.noreply.github.com>
2021-12-14 16:19:06 +0100

    STAC-15087: Dependencies cleanup and new artifactory url. (#251)
    * Dependencies cleanup and new artifactory url.
    
    * Fixed run gitlab local script
    
    * changed sts-aws tag from test_mysql job to be same as test_posgres job
    
    * Added sleep to docker_run

--- .setup-scripts/setup_artifactory_publishing.sh
+++ .setup-scripts/setup_artifactory_publishing.sh
@@ -1,12 +1,13 @@
 ## python artifactory dependency
+echo "Artifactory PyPI URL: $ARTIFACTORY_URL_PYPI"
 mkdir ~/.pip/ && touch ~/.pip/pip.conf
 echo "[global]" > ~/.pip/pip.conf
-echo "index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$ARTIFACTORY_URL/simple" >> ~/.pip/pip.conf
+echo "extra-index-url = https://$artifactory_user:$artifactory_password@$ARTIFACTORY_URL_PYPI" >> ~/.pip/pip.conf
 
 touch ~/.pypirc
 echo "[distutils]" > ~/.pypirc
 echo "index-servers = local" >> ~/.pypirc
 echo "[local]" >> ~/.pypirc
-echo "repository: https://$ARTIFACTORY_URL" >> ~/.pypirc
-echo "username: $ARTIFACTORY_USER" >> ~/.pypirc
-echo "password: $ARTIFACTORY_PASSWORD" >> ~/.pypirc
+echo "repository: https://$ARTIFACTORY_URL_PYPI" >> ~/.pypirc
+echo "username: $artifactory_user" >> ~/.pypirc
+echo "password: $artifactory_password" >> ~/.pypirc
