5dd371646bb525eed4786a0ac1326cce51cb24e1
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2022-04-01 09:23:40 +0200

    import cleanup

--- agent_integration_sample/stackstate_checks/agent_integration_sample/agent_integration_sample.py
+++ agent_integration_sample/stackstate_checks/agent_integration_sample/agent_integration_sample.py
@@ -2,7 +2,6 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-# project
 import json
 
 from stackstate_checks.base import AgentCheck, AgentIntegrationInstance, MetricStream, MetricHealthChecks, \
