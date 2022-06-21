6df7fecc4241a4cfea41a0fe104cf640f6a3573d
TGCapra <tanja.gasparovic@ag04.com>
2022-06-21 15:13:57 +0200

    Splunk Telemetry Base cleanup to adapt for v2

--- splunk_base/stackstate_checks/splunk/telemetry/splunk_telemetry_base.py
+++ splunk_base/stackstate_checks/splunk/telemetry/splunk_telemetry_base.py
@@ -2,13 +2,29 @@ import time
 
 from stackstate_checks.base import AgentCheck
 from stackstate_checks.base.errors import CheckException
-from stackstate_checks.splunk.client import TokenExpiredException, FinalizeException
-from stackstate_checks.splunk.config.splunk_instance_config import time_to_seconds, take_required_field, get_utc_time
-from stackstate_checks.splunk.saved_search_helper.saved_search_helper import chunks
+from stackstate_checks.splunk.client import TokenExpiredException, FinalizeException,SplunkClient
+from stackstate_checks.splunk.config.splunk_instance_config import time_to_seconds, take_required_field, get_utc_time, SplunkSavedSearch, SplunkInstanceConfig, CommittableState
+from stackstate_checks.splunk.saved_search_helper.saved_search_helper import chunks, SavedSearches
 
 
 # TODO refactor class to reuse v2 classes
 
+class Instance(object):
+    INSTANCE_TYPE = "splunk"
+
+    def __init__(self, instance, init_config, default_settings):
+        self.instance_config = SplunkInstanceConfig(instance, init_config)
+        self.splunk_client = self._build_splunk_client()
+
+        # transform component and relation saved searches to SavedSearch objects
+        saved_searches = [SplunkSavedSearch(self.instance_config, saved_search_instance)
+                          for saved_search_instance in instance.get('saved_searches', [])]
+
+        self.saved_searches = SavedSearches(self.instance_config, self.splunk_client, saved_searches)
+
+    # Hook to allow for mocking
+    def _build_splunk_client(self):
+        return SplunkClient(self.instance_config)
 
 class SplunkTelemetryBase(AgentCheck):
     SERVICE_CHECK_NAME = None  # must be set in the subclasses
@@ -24,73 +40,42 @@ class SplunkTelemetryBase(AgentCheck):
         self.status = None
         self.load_status()
 
+        # Hook to override instance creation
+    def _build_instance(self, instance):
+        return Instance(instance, self.init_config)
+
     def check(self, instance):
-        authentication = None
-        if 'url' not in instance:
-            raise CheckException('Splunk event/metric instance missing "url" value.')
-        if 'username' in instance and 'password' in instance and 'authentication' not in instance:
-            self.log.warning("This username and password configuration will be deprecated soon. Please use the new "
-                             "updated configuration from the conf")
-        if 'authentication' in instance:
-            authentication = instance["authentication"]
-            if 'basic_auth' not in authentication and 'token_auth' not in authentication:
-                raise CheckException('Splunk event/metric instance missing "authentication.basic_auth" or '
-                                     '"authentication.token_auth" value')
-            if 'basic_auth' in authentication:
-                basic_auth = authentication["basic_auth"]
-                if 'username' not in basic_auth:
-                    raise CheckException('Splunk event/metric instance missing "authentication.basic_auth.username" '
-                                         'value')
-                if 'password' not in basic_auth:
-                    raise CheckException('Splunk event/metric instance missing "authentication.basic_auth.password" '
-                                         'value')
-            if 'token_auth' in authentication:
-                token_auth = authentication["token_auth"]
-                if 'name' not in token_auth:
-                    raise CheckException('Splunk event/metric instance missing "authentication.token_auth.name" value')
-                if 'initial_token' not in token_auth:
-                    raise CheckException('Splunk event/metric instance missing '
-                                         '"authentication.token_auth.initial_token" value')
-                if 'audience' not in token_auth:
-                    raise CheckException('Splunk event/metric instance missing "authentication.token_auth.audience" '
-                                         'value')
 
-        current_time = self._current_time_seconds()
-        url = instance["url"]
-        if url not in self.instance_data:
-            self.instance_data[url] = self.get_instance(instance, current_time)
+        if self.instance_data is None:
+            self.instance_data = self._build_instance(instance)
 
-        instance = self.instance_data[url]
-        if not instance.initial_time_done(current_time):
-            self.log.debug("Skipping splunk metric/event instance %s, waiting for initial time to expire" % url)
-            return
+        committable_state = CommittableState(self.commit_state, self.load_state(instance))
+
+        instance = self.instance_data
+
+# snapshot needed or not? self.snapshot = bool(instance.get('snapshot', True))
 
         try:
-            if authentication and 'token_auth' in authentication:
-                self.log.debug("Using token based authentication mechanism")
-                base_url = instance.instance_config.base_url
-                self._token_auth_session(instance, authentication, base_url, self.status, self.persistence_check_name)
-            else:
-                self.log.debug("Using basic authentication mechanism")
-                self._auth_session(instance)
+            instance.splunk_client.auth_session(committable_state)
 
-            saved_searches = self._saved_searches(instance)
-            instance.saved_searches.update_searches(self.log, saved_searches)
+            def _service_check(status, tags=None, hostname=None, message=None):
+                self.service_check(self.SERVICE_CHECK_NAME, status, tags, hostname, message)
 
-            # after updating the regex searches in the saved search list, load the state and check
-            self.load_status()
-            instance.update_status(current_time, self.status)
+            def _process_data(saved_search, response):
+                return self._extract_telemetry(instance, response)
 
-            executed_searches = False
-            for saved_searches in chunks(instance.saved_searches.searches, instance.saved_searches_parallel):
-                executed_searches |= self._dispatch_and_await_search(instance, saved_searches)
+            instance.saved_searches.run_saved_searches(_process_data, _service_check, self.log, committable_state)
 
-            if len(instance.saved_searches.searches) != 0 and not executed_searches:
-                raise CheckException("No saved search was successfully executed.")
+            current_time = self._current_time_seconds()
+            url = instance["url"]
+            if url not in self.instance_data:
+                self.instance_data[url] = self.get_instance(instance, current_time)
 
-            # If no service checks were produced, everything is ok
-            if len(self.service_checks) is 0:
-                self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK)
+            instance = self.instance_data[url]
+            if not instance.initial_time_done(current_time):
+                self.log.debug("Skipping splunk metric/event instance %s, waiting for initial time to expire" % url)
+                return
+
         except TokenExpiredException as e:
             self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL, tags=instance.tags, message=str(e.message))
             self.log.exception("Splunk event/metric exception: %s" % str(e.message))
@@ -107,69 +92,6 @@ class SplunkTelemetryBase(AgentCheck):
     def _log_warning(self, instance, msg):
         self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.WARNING, tags=instance.tags, message=msg)
         self.log.warn(msg)
-
-    def _dispatch_and_await_search(self, instance, saved_searches):
-        start_time = self._current_time_seconds()
-        search_ids = []
-
-        for saved_search in saved_searches:
-            try:
-                # a unique saved search key to persist the data
-                persist_status_key = instance.instance_config.base_url + saved_search.name
-                if self.status.data.get(persist_status_key) is not None:
-                    sid = self.status.data[persist_status_key]
-                    self._finalize_sid(instance, sid, saved_search)
-                    self.update_persistent_status(instance.instance_config.base_url, saved_search.name, sid, 'remove')
-                sid = self._dispatch_saved_search(instance, saved_search)
-                self.update_persistent_status(instance.instance_config.base_url, saved_search.name, sid, "add")
-                search_ids.append((sid, saved_search))
-            except FinalizeException as e:
-                self.log.exception("Got an error %s while finalizing the saved search %s" % (e.message, saved_search.name))
-                if not instance.instance_config.ignore_saved_search_errors:
-                    raise e
-                self.log.warning("Ignoring the finalize exception as ignore_saved_search_errors flag is true")
-            except Exception as e:
-                self._log_warning(instance, "Failed to dispatch saved search '%s' due to: %s" % (saved_search.name, e.message))
-
-        executed_searches = False
-        for (sid, saved_search) in search_ids:
-            try:
-                self.log.debug("Processing saved search: %s." % saved_search.name)
-                count = self._process_saved_search(sid, saved_search, instance)
-                duration = self._current_time_seconds() - start_time
-                self.log.debug("Save search done: %s in time %d with results %d" % (saved_search.name, duration, count))
-                executed_searches = True
-            except Exception as e:
-                self._log_warning(instance, "Failed to execute dispatched search '%s' with id %s due to: %s" % (saved_search.name, sid, e.message))
-
-        return executed_searches
-
-    def _process_saved_search(self, search_id, saved_search, instance):
-        produced_count = 0
-        fail_count = 0
-
-        sent_events = saved_search.last_observed_telemetry
-        saved_search.last_observed_telemetry = set()
-
-        for response in self._search(search_id, saved_search, instance):
-            for message in response['messages']:
-                if message['type'] != "FATAL":
-                    if message['type'] == "INFO" and message['text'] == "No matching fields exist":
-                        self.log.info("Saved search %s did not produce any data." % saved_search.name)
-                    else:
-                        self.log.info("Received unhandled message on saved search %s, got: '%s'." % (saved_search.name, str(message)))
-
-            for data_point in self._extract_telemetry(saved_search, instance, response, sent_events):
-                if data_point is None:
-                    fail_count += 1
-                else:
-                    self._apply(**data_point)
-                    produced_count += 1
-
-        if fail_count > 0:
-            self._log_warning(instance, "%d telemetry records failed to process when running saved search '%s'" % (fail_count, saved_search.name))
-
-        return produced_count
 
     def _extract_telemetry(self, saved_search, instance, result, sent_already):
         for data in result["results"]:
@@ -205,6 +127,7 @@ class SplunkTelemetryBase(AgentCheck):
                 self.log.exception(e)
                 yield None
 
+# do we use it?
     def _apply(self, **kwargs):
         """ How the telemetry info should be sent by the check, e.g., as event, guage, etc. """
         raise NotImplementedError
@@ -216,81 +139,6 @@ class SplunkTelemetryBase(AgentCheck):
             for key, value in data.iteritems()
             if self._include_as_tag(key)
         }
-
-    def commit_succeeded(self, instance):
-        instance = self.instance_data[instance["url"]]
-        status_dict, continue_after_commit = instance.get_status()
-        self.update_persistent_status(instance.instance_config.base_url, None, status_dict, 'add')
-        return continue_after_commit
-
-    def commit_failed(self, instance):
-        """
-        Upon failure we do not commit the new timestamps.
-        """
-        pass
-
-    def _dispatch_saved_search(self, instance, saved_search):
-        """
-        Initiate a saved search, returning the search id
-        :param instance: Configuration of the splunk instance
-        :param saved_search: Configuration of the saved search
-        :return:
-        """
-        parameters = saved_search.parameters
-        # json output_mode is mandatory for response parsing
-        parameters["output_mode"] = "json"
-
-        earliest_epoch_datetime = get_utc_time(saved_search.last_observed_timestamp)
-        splunk_user = instance.instance_config.username
-        splunk_app = saved_search.app
-        ignore_saved_search_errors = instance.instance_config.ignore_saved_search_errors
-
-        parameters["dispatch.time_format"] = self.TIME_FMT
-        parameters["dispatch.earliest_time"] = earliest_epoch_datetime.strftime(self.TIME_FMT)
-
-        if "dispatch.latest_time" in parameters:
-            del parameters["dispatch.latest_time"]
-
-        # Always observe the last time for data and use max query chunk seconds
-        latest_time_epoch = saved_search.last_observed_timestamp + saved_search.config['max_query_chunk_seconds']
-        current_time = self._current_time_seconds()
-
-        if latest_time_epoch >= current_time:
-            self.log.info("Caught up with old splunk data for saved search %s since %s" % (saved_search.name, parameters["dispatch.earliest_time"]))
-            saved_search.last_recover_latest_time_epoch_seconds = None
-        else:
-            saved_search.last_recover_latest_time_epoch_seconds = latest_time_epoch
-            latest_epoch_datetime = get_utc_time(latest_time_epoch)
-            parameters["dispatch.latest_time"] = latest_epoch_datetime.strftime(self.TIME_FMT)
-            self.log.info("Catching up with old splunk data for saved search %s from %s to %s " % (saved_search.name, parameters["dispatch.earliest_time"],parameters["dispatch.latest_time"]))
-
-        self.log.debug("Dispatching saved search: %s starting at %s." % (saved_search.name, parameters["dispatch.earliest_time"]))
-
-        return self._dispatch(instance, saved_search, splunk_user, splunk_app, ignore_saved_search_errors, parameters)
-
-    def _auth_session(self, instance):
-        """ This method is mocked for testing. Do not change its behavior """
-        instance.splunkHelper.auth_session()
-
-    def _token_auth_session(self, instance, authentication, base_url, status, persistence_check_name):
-        """ This method is mocked for testing. Do not change its behavior """
-        return instance.splunkHelper.token_auth_session(authentication, base_url, status, persistence_check_name)
-
-    def _dispatch(self, instance, saved_search, splunk_user, splunk_app, ignore_saved_search_errors, parameters):
-        """ This method is mocked for testing. Do not change its behavior """
-        return instance.splunkHelper.dispatch(saved_search, splunk_user, splunk_app, ignore_saved_search_errors, parameters)
-
-    def _finalize_sid(self, instance, sid, saved_search):
-        """ This method is mocked for testing. Do not change its behavior """
-        return instance.splunkHelper.finalize_sid(sid, saved_search)
-
-    def _saved_searches(self, instance):
-        """ This method is mocked for testing. Do not change its behavior """
-        return instance.splunkHelper.saved_searches()
-
-    def _search(self, search_id, saved_search, instance):
-        """ This method is mocked for testing. Do not change its behavior """
-        return instance.splunkHelper.saved_search_results(search_id, saved_search)
 
     def _current_time_seconds(self):
         """ This method is mocked for testing. Do not change its behavior """
