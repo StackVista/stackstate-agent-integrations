44ebbace0e2d33700535116dbc5c815bb6b96e65
Zandré Witte <zwitte@stackstate.com>
2022-07-01 13:02:44 +0200

    conda env settings

--- .setup-scripts/conda_env.ps1
+++ .setup-scripts/conda_env.ps1
@@ -8,11 +8,9 @@ if (($env_name -ne $null) -and ($env_name -eq  $envName)) {
   Write-Output "Virtual Environment '$envName' already exists"
   return
 }
-$DD_PIP_VERSION = '19.1'
-$DD_SETUPTOOLS_VERSION = '41.0.1'
+$DD_PIP_VERSION = '22.1.2'
+$DD_SETUPTOOLS_VERSION = '44.1.1'
 conda create -n $envName python python=$pythonVersion -y
 conda activate $envName
 pip install -i https://pypi.python.org/simple pip==$DD_PIP_VERSION
 pip install --ignore-installed setuptools==$DD_SETUPTOOLS_VERSION
-pip install invoke distro=='1.4.0' awscli=='1.16.240'
-pip install -r requirements.txt
