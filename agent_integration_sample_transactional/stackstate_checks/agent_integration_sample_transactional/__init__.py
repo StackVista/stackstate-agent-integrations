77f369a38e0eec4b2d5f98238512e79f9d03b2f3
Melcom van Eeden <melcomve@outlook.com>
2022-07-20 11:55:49 +0200

    Attempt to create a new agent sample integration check for transactional state

---
+++ agent_integration_sample_transactional/stackstate_checks/agent_integration_sample_transactional/__init__.py
@@ -0,0 +1,10 @@
+# (C) StackState 2020
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from .__about__ import __version__
+from .agent_integration_sample_transactional import AgentIntegrationSampleTransactionalCheck
+
+__all__ = [
+    '__version__',
+    'AgentIntegrationSampleTransactionalCheck'
+]
