deec21da3c8ec0465aad5939748668d5e4c85af9
Louis Parkin <lparkin@stackstate.com>
2025-03-17 11:30:44 +0100

    STAC-21521 Debugging test failures. [2]

--- dynatrace_topology/tests/test_dynatrace_tags.py
+++ dynatrace_topology/tests/test_dynatrace_tags.py
@@ -7,11 +7,11 @@ def test_tags(dynatrace_check, requests_mock, aggregator, topology):
     """
     Testing do dynatrace tags finish in right places
     """
-    set_http_responses(requests_mock, hosts=read_file('HOST-9106C06F228CEC6B.json', 'samples'))
+    set_http_responses(requests_mock, hosts=read_file('HOST-27D021F0FED92055.json', 'samples'))
     dynatrace_check.run()
     aggregator.assert_service_check(dynatrace_check.SERVICE_CHECK_NAME, count=1, status=AgentCheck.OK)
     components = topology.get_snapshot(dynatrace_check.check_id)['components']
-    assert components[0]['data']['environments'] == ['test-environment']
-    assert components[0]['data']['domain'] == 'test-domain'
-    assert components[0]['data']['layer'] == 'test-layer'
-    assert 'test-identifier' in components[0]['data']['identifiers']
+    assert components[0]['data']['environments'] == ['production']
+    assert components[0]['data']['domain'] == 'dynatrace'
+    assert components[0]['data']['entityId'] == 'HOST-27D021F0FED92055'
+    assert 'urn:dynatrace:/HOST-27D021F0FED92055' in components[0]['data']['identifiers']
