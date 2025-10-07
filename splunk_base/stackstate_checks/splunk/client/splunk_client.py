ad2d1dca8dd88163961c54a91498d1e94b4807c9
Alessio Biancalana <alessio.biancalana@suse.com>
2025-10-07 11:53:46 +0200

    STAC-23320: remove trailing whitespace

--- splunk_base/stackstate_checks/splunk/client/splunk_client.py
+++ splunk_base/stackstate_checks/splunk/client/splunk_client.py
@@ -113,7 +113,7 @@ class SplunkClient:
                 is_initial_token):
             self.log.debug("The token needs renewal as token is about to expire or this is initial token")
             new_token = self._create_auth_token(token)
-            # Only commit the token 
+            # Only commit the token
             if os.getenv("COMMIT_JWT_TOKEN_STATE", True):
                 committable_state.set_auth_token(new_token)
         # Update the Authorization header falling back on the original token provided
