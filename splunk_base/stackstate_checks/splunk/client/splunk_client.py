8b2ab27bb09b7c1ef410372c897a75b54f17c8bd
Alessio Biancalana <alessio.biancalana@suse.com>
2025-10-07 11:31:12 +0200

    STAC-23320: put the old token in x-backend-auth

--- splunk_base/stackstate_checks/splunk/client/splunk_client.py
+++ splunk_base/stackstate_checks/splunk/client/splunk_client.py
@@ -96,9 +96,10 @@ class SplunkClient:
     def _token_auth_session(self, committable_state):
         is_initial_token = False
         token = committable_state.get_auth_token()
+        new_token = ""
         if token is None:
             # Since this is first time run, pick the token from conf.yaml
-            token = self.instance_config.initial_token
+            token = os.getenv("INITIAL_TOKEN", self.instance_config.initial_token)
             is_initial_token = True
 
         if self.jwt_adapter.is_token_expired(token, is_initial_token):
@@ -111,19 +112,20 @@ class SplunkClient:
                 self.instance_config.renewal_days,
                 is_initial_token):
             self.log.debug("The token needs renewal as token is about to expire or this is initial token")
-            token = self._create_auth_token(token)
-            committable_state.set_auth_token(token)
-        self.requests_session.headers.update({'Authorization': "Bearer %s" % token})
+            new_token = self._create_auth_token(token)
+            # Only commit the token 
+            if os.getenv("COMMIT_JWT_TOKEN_STATE", True):
+                committable_state.set_auth_token(new_token)
+        # Update the Authorization header falling back on the original token provided
+        self.requests_session.headers.update({'Authorization': "Bearer %s" % (new_token or token)})
         self.requests_session.headers.update({'x-backend-auth': "Bearer %s" % token})
 
     def _create_auth_token(self, token):
         self.log.debug("Creating a new authentication token")
         self.requests_session.headers.update({'Authorization': "Bearer %s" % token})
-        self.requests_session.headers.update({'x-backend-auth': "Bearer %s" % token})
 
         new_token = self.jwt_adapter.generate_token()
         self.requests_session.headers.update({'Authorization': "Bearer %s" % new_token})
-        self.requests_session.headers.update({'x-backend-auth': "Bearer %s" % new_token})
         return new_token
 
     def _get_saved_search_path(self, splunk_ns_user, splunk_app=None):
